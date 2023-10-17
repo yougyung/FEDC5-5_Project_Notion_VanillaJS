e5ab8e1465b9fc4c256317f147fe5fb1421d34bb
SoJuSo <asdfg7123@gmail.com>
2023-10-17 11:10:23 +0900

    [Feat] utils 폴더에 프로젝트에 필요한 localstorage와 api 호출 기능 추가

---
+++ src/utils/api.js
@@ -0,0 +1,45 @@
+export const API_END_POINT = "https://kdt-frontend.programmers.co.kr";
+export const DEFAULT_USERNAME = "ksj-notion-cloning";
+
+// 일단 더미데이터로 실험해보고 이후에 API 연결하기
+export const DUMMY_DATA = [
+  {
+    id: 1, // Document id
+    title: "노션을 만들자", // Document title
+    documents: [
+      {
+        id: 2,
+        title: "블라블라",
+        documents: [
+          {
+            id: 3,
+            title: "함냐함냐",
+            documents: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "hello!",
+    documents: [],
+  },
+];
+
+export const request = async () => {
+  try {
+    const res = await fetch(`${API_END_POINT}/documents`, {
+      headers: {
+        "Content-Type": "application/json",
+        "x-username": `${DEFAULT_USERNAME}`,
+      },
+    });
+    if (res.ok) {
+      return await res.json();
+    }
+    throw new Error("API 처리 중 오류 발생!!");
+  } catch (error) {
+    alert(error.message);
+  }
+};
