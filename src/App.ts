0bf155b050255eb408f7af310d479531931206a4
Kim-Jaemin420 <jany240@naver.com>
2023-10-17 16:46:48 +0900

    Feat: App 컴포넌트 UI 구현

--- src/App.ts
+++ src/App.ts
@@ -1,7 +1,22 @@
+import { createComponent } from "./core";
+import { Document, Sidebar } from "./components";
+import styles from "./app.module.scss";
+
+const { s_container } = styles;
+
 function App() {
+  const documentComponent = createComponent(Document);
+  const sidebarComponent = createComponent(Sidebar);
+
   return {
     element: `
-      <di>노션 클로닝 어플리케이션입니다.</di>
+      <div class=${s_container}>
+        <h1 class="a11y-hidden">
+          노션 클로닝 어플리케이션
+        </h1>
+        ${sidebarComponent.element}
+        ${documentComponent.element}
+      </div>
     `,
   };
 }
