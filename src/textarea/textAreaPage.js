76f60b46b8713746a5795ab3ba661f472149186e
SoJuSo <asdfg7123@gmail.com>
2023-10-17 20:51:20 +0900

    [Style] 인라인 코드를 css파일로 통합 및 일부 수정

--- src/textarea/textAreaPage.js
+++ src/textarea/textAreaPage.js
@@ -6,21 +6,21 @@ export default function TextAreaPage({ $target, initialState }) {
   $textArea.className = "textArea";
 
   const $titleInputArea = $.createElement("input");
+  $titleInputArea.className = "textArea-title";
   $titleInputArea.type = "text";
   $titleInputArea.name = "title";
-  $titleInputArea.style = "width:600px";
   $titleInputArea.value = DUMMY_DATA_TEXT_CONTENT.title;
   // <input type="text" name="title" style="width:600px;" value="${this.state.title}" />
   // <textarea name="content" style="width:600px;height:400px;"
   const $contentTextArea = $.createElement("textarea");
+  $contentTextArea.className = "textArea-content";
   $contentTextArea.name = "content";
-  $contentTextArea.style = "width:600px;height:400px;";
   $contentTextArea.value = DUMMY_DATA_TEXT_CONTENT.content;
 
   $textArea.appendChild($titleInputArea);
   $textArea.appendChild($contentTextArea);
 
   $target.appendChild($textArea);
-  console.log($target);
-  console.log(DUMMY_DATA_TEXT_CONTENT.title);
+  // console.log($target);
+  // console.log(DUMMY_DATA_TEXT_CONTENT.title);
 }
