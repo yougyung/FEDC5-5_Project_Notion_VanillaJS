a214c87b43d674cf4e7faf97cd5c96903124ff07
dy6578ekdbs <dy6578ekdbs@gmail.com>
2023-10-17 15:28:30 +0900

    🐛 fix : route 함수 로직 변경

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import HomePage from "./pages/HomePage.js";
 import { initRouter } from "./utils/router.js";
-import { DUMMY_DATA } from "./mock.js";
-import Sidebar from "./components/Sidebar.js";
+import {
+  DUMMY_DATA,
+  DUMMY_HEADER_TABS,
+  DUMMY_SINGLE_POST_DATA,
+} from "./mock.js";
+
+import Sidebar from "./components/Sidebar/Sidebar.js";
+import EditorPage from "./components/EditorPage/EditorPage.js";
 
 export default function App({ $target }) {
   const sidebar = new Sidebar({ $target, initialState: DUMMY_DATA });
+  const editorPage = new EditorPage({
+    $target,
+    initialState: DUMMY_SINGLE_POST_DATA,
+  });
 
   /** SPA 라우팅 */
   this.route = () => {
     console.log("화면 이동 감지");
 
-    // $target.innerHTML = ""; // 화면 비우기
+    //$target.innerHTML = ""; // 화면 비우기
     const { pathname } = window.location;
 
-    // path 위치에 따른 컴포넌트 렌더링
+    // root 접속 시 - 선택 안된 상황
     if (pathname === "/") {
-      console.log("/");
       sidebar.setState();
-    } else if (pathname.indexOf("/posts/") === 0) {
-      //const [, , postId] = pathname.split("/");
-      //postEditPage.setState({ postId });
+    } else if (pathname.indexOf("/documents/") === 0) {
+      const [, , documentId] = pathname.split("/");
+      console.log(documentId);
+      editorPage.setState(documentId);
     }
   };
 
