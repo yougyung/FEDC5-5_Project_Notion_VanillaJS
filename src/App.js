9c95f481697919a9ea68d87acd7f577eeec43496
mibu119 <horang296@gmail.com>
2023-10-17 19:01:54 +0900

    ✨ sidebar에 documents 불러오기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { request } from "./api.js";
+import Sidebar from "./sidebar/Sidebar.js";
 
 export default function App({ $target }) {
   const $app = document.createElement("section");
-  $app.textContent = "Hello World!";
   $target.appendChild($app);
 
+  const sidebar = new Sidebar({
+    $target,
+    initialState: [],
+  });
+
   const fetchDocuments = async () => {
-    const result = await request("");
-    console.log(result);
+    const documents = await request("");
+    sidebar.setState(documents);
   };
 
   fetchDocuments();
--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -1,1 +1,26 @@
-export default function Sidebar({ $target }) {}
+export default function Sidebar({ $target, initialState }) {
+  const $siderbar = document.createElement("aside");
+  $target.appendChild($siderbar);
+
+  this.state = initialState;
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+
+  this.render = () => {
+    $siderbar.innerHTML = `
+            <ul>
+                ${this.state
+                  .map(
+                    (document) =>
+                      `<li data-id=${document.id}>${document.title}</li>`
+                  )
+                  .join("")}
+            </ul>
+        `;
+  };
+
+  this.render();
+}
