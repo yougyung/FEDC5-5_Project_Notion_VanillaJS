58cd79adee077e9e9b73e9dbfda2f280abfca12c
dy6578ekdbs <dy6578ekdbs@gmail.com>
2023-10-17 15:26:59 +0900

    🐛 chore : 라우팅 함수 이름을 navigate로 변경

--- src/utils/router.js
+++ src/utils/router.js
@@ -1,8 +1,8 @@
 const ROUTE_CHANGE_EVENT_NAME = "route-change";
 
-export const initRouter = (onRoute) => {
+export const initRouter = onRoute => {
   /** 커스텀 이벤트 */
-  window.addEventListener(ROUTE_CHANGE_EVENT_NAME, (e) => {
+  window.addEventListener(ROUTE_CHANGE_EVENT_NAME, e => {
     const { nextUrl } = e.detail;
     if (nextUrl) {
       history.pushState(null, null, nextUrl);
@@ -12,12 +12,12 @@ export const initRouter = (onRoute) => {
 };
 
 /** 함수 이름 바꿔야지,,, */
-export const push = (nextUrl) => {
+export const navigate = nextUrl => {
   window.dispatchEvent(
     new CustomEvent("route-change", {
       detail: {
         nextUrl,
       },
-    })
+    }),
   );
 };
