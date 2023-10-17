763791fc6dab38acc4c05ed92a0fc17767443f6a
Kim-Jaemin420 <jany240@naver.com>
2023-10-17 16:46:30 +0900

    Feat: document 컴포넌트 UI 구현

---
+++ src/components/Document.ts
@@ -0,0 +1,11 @@
+function Document() {
+  return {
+    element: `
+      <form class="document">
+        <textarea rows=10 cols=100></textarea>
+      </form>
+    `,
+  };
+}
+
+export default Document;
