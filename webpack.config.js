e7a264ca9a5befd65bf10263b20a41ecb1a81c2e
Kim-Jaemin420 <jany240@naver.com>
2023-10-17 17:08:43 +0900

    Chore: dotenv 사용을 위해 dotenv-webpack 설치 및 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: "./src/index.ts",
@@ -52,5 +53,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./index.html",
     }),
+    new Dotenv(),
   ],
 };
