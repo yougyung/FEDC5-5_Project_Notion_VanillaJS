b0a76ff0be2920298a0c5ebf6f6af96138e87d28
Kim-Jaemin420 <jany240@naver.com>
2023-10-17 13:38:25 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_module/,
+        use: {
+          loader: "ts-loader",
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: "./dist",
+    port: 8080,
+    hot: true,
+  },
+  resolve: {
+    modules: [path.join(__dirname, "src"), "node_modules"],
+    extensions: [".ts", ".js"],
+    alias: {
+      "@": path.resolve(__dirname, "src/"),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+};
