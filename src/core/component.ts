7b4c37db48445ab43be69c19cd93e3711818f809
Kim-Jaemin420 <jany240@naver.com>
2023-10-17 13:42:50 +0900

    Feat: 컴포넌트 생성 함수 구현

---
+++ src/core/component.ts
@@ -0,0 +1,51 @@
+interface ComponentInstance {
+  element: string;
+  bindEvents?: () => void;
+}
+
+interface CurrentComponent {
+  id: string;
+  stateIndex: number;
+}
+
+const hasSigleRoot = (componentElements: string) => {
+  const $container = document.createElement("div");
+  $container.innerHTML = componentElements.trim();
+
+  return $container.childNodes.length === 1;
+};
+
+export let currentComponent: CurrentComponent | null = null;
+
+function createComponent<T>(
+  component: (props: T) => ComponentInstance,
+  props: T,
+): ComponentInstance;
+
+function createComponent(component: () => ComponentInstance): ComponentInstance;
+
+function createComponent<T>(
+  component: (props?: T) => ComponentInstance,
+  props?: T,
+) {
+  const previousComponent = currentComponent;
+
+  currentComponent = { id: component.name, stateIndex: 0 };
+
+  const componentInstance = component(props);
+
+  if (!hasSigleRoot(componentInstance.element)) {
+    throw new Error(`컴포넌트가 하나의 상위 요소로 감싸져 있지 않습니다!`);
+  }
+
+  const idAttributeAddedElement = componentInstance.element.replace(
+    /(<\w+)(\s|>)/,
+    `$1 id="${currentComponent.id}"$2`,
+  );
+
+  currentComponent = previousComponent;
+
+  return { ...componentInstance, render: idAttributeAddedElement };
+}
+
+export { createComponent };
