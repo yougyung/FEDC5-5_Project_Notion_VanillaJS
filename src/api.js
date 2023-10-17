c8ca772d51c2db3b22ee1dd4f3bd6ae48988535e
lunarmoon7 <49crehbgr@gmail.com>
2023-10-17 10:37:00 +0900

    feat: 기본 API 호출 코드 추가

---
+++ src/api.js
@@ -0,0 +1,20 @@
+export const API_END_POINT = 'https://kdt-frontend.programmers.co.kr/documents';
+
+export const request = async (url='', options = {}) => {
+  try {
+    const response = await fetch(`${API_END_POINT}${url}`, {
+      ...options,
+      headers: {
+        'x-username': 'zentechie',
+      },
+    });
+
+    if (response.ok) {
+      return await response.json();
+    }
+
+    throw new Error('API 호출에 실패했습니다!');
+  } catch (e) {
+    console.log(e.message);
+  }
+};
