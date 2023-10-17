0aa69b36eae9b38247c1d738d6b50c421019392d
Kim-Jaemin420 <jany240@naver.com>
2023-10-18 00:33:55 +0900

    Feat: 문서 하나의 타입과 자식 문서 타입에 대해 선언

--- src/types/document.ts
+++ src/types/document.ts
@@ -3,3 +3,15 @@ export interface Document {
   title: string;
   documents: Document[];
 }
+
+export interface DocumentOverview {
+  id: number;
+  title: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface DocumentDetail extends DocumentOverview {
+  content: string;
+  documents: DocumentOverview[];
+}
