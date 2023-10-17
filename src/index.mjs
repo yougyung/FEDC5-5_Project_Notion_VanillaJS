2c6a2569daad5d8a7a7209444ea036138a202a08
dy6578ekdbs <dy6578ekdbs@gmail.com>
2023-10-17 15:28:15 +0900

    💄 style : main에 flex 속성 추가

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,7 @@
 import App from "./App.js";
+import style from "./global.module.css";
 
 const $target = document.querySelector("#app");
+$target.className = style.container;
 
 new App({ $target });
