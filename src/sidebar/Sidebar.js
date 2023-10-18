368643165b7462e79063674181a26ab8a8149bb5
mibu119 <horang296@gmail.com>
2023-10-18 12:17:24 +0900

    🚚 sidebarheader sidebar 컴포넌트로 이동

--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -1,9 +1,15 @@
 import { request } from "../api.js";
 import DocumentList from "./DocumentList.js";
+import SidebarHeader from "./SidebarHeader.js";
 
 export default function Sidebar({ $target }) {
   const $sidebar = document.createElement("aside");
   $target.appendChild($sidebar);
+
+  new SidebarHeader({
+    $target: $sidebar,
+    username: "Roto",
+  });
 
   const sidebar = new DocumentList({
     $target: $sidebar,
