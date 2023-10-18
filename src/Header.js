136c3022596c6e874303573a2be70cc16e980e1f
hellosonic-r <hellommmh@gmail.com>
2023-10-18 14:20:44 +0900

    feat : 헤더 컴포넌트 생성

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+export default function Header({$target, title}) {
+    const $h1 = document.createElement('h1');
+    $target.appendChild($h1);
+
+    this.render = () => {
+        $h1.innerText = `${title}`;
+    }
+
+    this.render();
+}
