7af4651d363764a2a69fdc70f97df22449cf99ad
Kim-Jaemin420 <jany240@naver.com>
2023-10-18 13:49:34 +0900

    Feat: 에디터 컴포넌트 접근성에 맞게 태그 사용

--- src/components/Editor/Editor.ts
+++ src/components/Editor/Editor.ts
@@ -1,8 +1,18 @@
+import styles from "./editor.module.scss";
+
+const { s_editorForm, s_editorInput, s_editorContent } = styles;
+
 function Editor() {
   return {
     element: `
-      <form>
-        <textarea rows=10 cols=100></textarea>
+      <form class=${s_editorForm}>
+        <fieldset>
+          <legend class="a11yHidden">새 문서 작성</legend>
+          <label for="title">제목</label>
+          <input id="title" type="text" class=${s_editorInput}/>
+          <label for="content">내용</label>
+          <textarea id="content" rows=50 class=${s_editorContent}></textarea>
+        </fieldset>
       </form>
     `,
   };
