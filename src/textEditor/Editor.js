c0abadcc81d77c1621e942b56c64f1dbb14a0038
mibu119 <horang296@gmail.com>
2023-10-18 10:37:06 +0900

    🌱 Editor 컴포넌트 구성

--- src/textEditor/Editor.js
+++ src/textEditor/Editor.js
@@ -1,1 +1,23 @@
-export default function Editor({ $target }) {}
+export default function Editor({ $target, initialState }) {
+  const $editor = document.createElement("section");
+  $target.appendChild($editor);
+
+  this.state = {
+    title: "",
+    content: "",
+  };
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+
+  this.render = () => {
+    $editor.innerHTML = `
+            <input type="text" placeholder="제목" value="${this.state.title}" />
+            <textarea>${this.state.content}</textarea>
+        `;
+  };
+
+  this.render();
+}
