3e381859b2ddccfdd83ff8f161a9613da670a806
SoJuSo <asdfg7123@gmail.com>
2023-10-19 10:35:28 +0900

    [Feat] 기초 라우팅 처리 구현 및 예외상황에서 다시 본문으로 돌아가는 a태그 추가

--- src/app.js
+++ src/app.js
@@ -62,26 +62,48 @@ export default function App({ $target }) {
 
     // this.state[0].id
     // 뭔가 멋이 없어...간지가 나지 않아..
-    await fetchSelectedDocs(this.state[0].id);
+    // await fetchSelectedDocs(this.state[0].id);
   };
 
   const fetchSelectedDocs = async (id) => {
     // 흠...그러면 처음 루트페이지 불러오고 그다음에 얘를 실행해야하는게 순서상 맞다. 안그러면 오류 날 것 같은디?
     // 처음 있는 값을 일단 루트로 둔다. 하지만 이 부분은 localStorage 기능을 이용해서 추후에 이전에 열고 있던 페이지를 루트로 보여주면 좋을 것 같다.
     const nowDocsId = id;
-    if (nowDocsId) {
+    if (nowDocsId !== 0) {
       //방어코드
       const selectedDocs = await request(`/documents/${nowDocsId}`);
       textAreaRender.setState(selectedDocs);
+    } else {
+      console.error(`nowDocsId의 값이 비어있거나 숫자가 아닙니다!! nowDocsId === ${nowDocsId}`);
     }
   };
 
   this.render = async () => {};
 
-  const init = async () => {
-    await fetchRootDocs();
-    // console.log(this.state);
+  this.route = async () => {
+    const { pathname } = location;
+    console.log(pathname.indexOf("/documents/"));
+    if (pathname === "/") {
+      // 루트인 경우
+      await fetchRootDocs();
+      // console.log(this.state);
+      // 일단 루트의 id를 입력하고 나중에는 localStorage로 마지막에 봤던 페이지를 출력해주자
+      await fetchSelectedDocs(this.state[0].id); //localstorage에서 뭔가를 해보자
+    } else if (pathname.indexOf("/documents/") > -1) {
+      const [, , Id] = pathname.split("/");
+      console.log(Id);
+      await fetchRootDocs();
+      await fetchSelectedDocs(Id);
+    } else {
+      // 404처리
+      console.log(pathname);
+      $.querySelector("body").innerHTML = `
+      <h1 style="display:block">404 Not Found!</h1>
+      <span style="display:block">${pathname} 페이지는 존재하지 않습니다!!</span>
+      <a style="display:block" href="/">다시 메인 화면으로 돌아가려면 클릭하세요</a>
+      `;
+    }
   };
 
-  init();
+  this.route();
 }
