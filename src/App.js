faf8ab071d92f5a5639775806be1e3f024975852
dy6578ekdbs <dy6578ekdbs@gmail.com>
2023-10-19 11:33:49 +0900

    ♻️ refactor : 최상단 App 컴포넌트로 모든  state 옮김

--- src/App.js
+++ src/App.js
@@ -7,34 +7,83 @@ import {
 
 import Sidebar from "./components/Sidebar/Sidebar.js";
 import EditorPage from "./components/EditorPage/EditorPage.js";
+import Header from "./components/Header/Header.js";
+import Modal from "./components/CreateModal/Modal.js";
+import { getPost, getPostList } from "./utils/api.js";
 
 export default function App({ $target }) {
-  const sidebar = new Sidebar({ $target, initialState: DUMMY_DATA });
-  const editorPage = new EditorPage({
-    $target,
-    initialState: { documentId: "new" },
-  });
+  /**
+   * App에서 모든 State 관리
+   */
 
+  this.state = {
+    documents: [],
+    selectedDocument: DUMMY_SINGLE_POST_DATA,
+  };
+
+  this.setState = nextState => {
+    this.state = nextState;
+  };
+
+  // Sidebar
+  const sidebar = new Sidebar({ $target, initialState: DUMMY_DATA });
   sidebar.setState();
 
-  /** SPA 라우팅 */
-  // 위치를 editorPage로 내려야하나?
-  // 아님 그대로 여기에 두되,
-  this.route = () => {
-    console.log("화면 이동 감지");
+  // Editor
+  const editorPage = new EditorPage({
+    $target,
+    initialState: DUMMY_SINGLE_POST_DATA,
+  });
 
+  // Header
+  const header = new Header({ $target, initialState: [] });
+
+  // New Modal
+  const modal = new Modal({ $target, initialState: {} });
+
+  /** 문서 목록 가져오기 + state 변경 + 사이드바 리렌더링 */
+  const fetchDocuments = async () => {
+    const documents = await getPostList();
+    this.setState({
+      ...this.state,
+      documents,
+    });
+    sidebar.setState(this.state.documents);
+  };
+
+  /** 선택된 문서 가져오기 */
+  const fetchDocument = async documentId => {
+    const selectedDocument = await getPost(documentId);
+    this.setState({
+      ...this.state,
+      selectedDocument,
+    });
+
+    editorPage.setState(this.state.selectedDocument);
+  };
+
+  this.route = () => {
     //$target.innerHTML = ""; // 화면 비우기
     const { pathname } = window.location;
 
     // root 접속 시 - 선택 안된 상황
     if (pathname === "/") {
-      //sidebar.setState();
+      // 루트 ui 추가 필요
+      // editor가 있다면 지우기
     } else if (pathname.indexOf("/documents/") === 0) {
+      // 루트 ui 삭제 필요
       const [, , documentId] = pathname.split("/");
-      editorPage.setState(documentId);
+      fetchDocument(documentId);
+    } else if (pathname === "/new") {
+      editorPage.setState("new");
     }
   };
 
-  this.route();
-  initRouter(() => this.route());
+  const init = async () => {
+    await fetchDocuments();
+    this.route();
+    initRouter(() => this.route());
+  };
+
+  init();
 }
