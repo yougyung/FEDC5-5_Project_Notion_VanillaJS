e7e0b1f387537edc82b81e47f1f3da68fc6a1a92
mibu119 <horang296@gmail.com>
2023-10-19 21:54:56 +0900

    👽️ 외부 api에 content-type 추가

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,13 +7,16 @@ export const request = async (documentId, options = {}) => {
       {
         ...options,
         headers: {
+          "Content-type": "application/json",
           "x-username": "roto",
         },
       }
     );
 
     if (response.ok) {
-      return await response.json();
+      const result = await response.json();
+      console.log("응답 결과:", result);
+      return result;
     }
 
     throw new Error("API 처리 중 에러가 발생했습니다.");
