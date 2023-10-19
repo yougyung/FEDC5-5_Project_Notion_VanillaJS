37ea7c98e390fc9ccdce5b2c8818301a25773b2f
mibu119 <horang296@gmail.com>
2023-10-19 12:10:35 +0900

    🔥 route 이동을 위한 id 추가, 불필요한 코드 제거

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Sidebar from "./sidebar/Sidebar.js";
 import DocumentEditPage from "./textEditor/DocumentEditPage.js";
+import { initRouter } from "./utils/router.js";
 
 export default function App({ $target }) {
   const sidebar = new Sidebar({
     $target,
-    initialState: [],
+    initialState: {
+      selectedDocumentId: null,
+    },
   });
 
   const documentEditPage = new DocumentEditPage({
@@ -17,4 +20,31 @@ export default function App({ $target }) {
       },
     },
   });
+
+  this.route = () => {
+    const { pathname } = window.location;
+
+    if (pathname === "/") {
+      return;
+    }
+
+    if (pathname.indexOf("/documents") !== 0) return;
+
+    const [, , documentId] = pathname.split("/");
+    documentEditPage.setState({
+      documentId: isNaN(documentId) ? documentId : parseInt(documentId),
+    });
+
+    if (!isNaN(documentId)) {
+      sidebar.setState({
+        selectedId: parseInt(documentId),
+      });
+    }
+  };
+
+  window.addEventListener("popstate", () => this.route());
+
+  this.route();
+
+  initRouter(() => this.route());
 }
