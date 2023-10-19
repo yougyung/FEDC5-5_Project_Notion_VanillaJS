e4b0ce1f0a7dc2598f3ae2ebe0e18acffaaf2455
mibu119 <horang296@gmail.com>
2023-10-19 09:54:50 +0900

    🚚 api.js utils 폴더로 이동

---
+++ src/utils/api.js
@@ -0,0 +1,23 @@
+const API_END_POINT = "https://kdt-frontend.programmers.co.kr";
+
+export const request = async (documentId, options = {}) => {
+  try {
+    const response = await fetch(
+      `${API_END_POINT}/documents${documentId ? `/${documentId}` : ""}`,
+      {
+        ...options,
+        headers: {
+          "x-username": "roto",
+        },
+      }
+    );
+
+    if (response.ok) {
+      return await response.json();
+    }
+
+    throw new Error("API 처리 중 에러가 발생했습니다.");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
