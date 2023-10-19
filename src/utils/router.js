e2f645568918d9a61be81a6ae014e13b0523a100
mibu119 <horang296@gmail.com>
2023-10-19 09:55:06 +0900

    🌱 router.js 파일 추가

---
+++ src/utils/router.js
@@ -0,0 +1,22 @@
+const ROUTE_CHANGE_EVENT_NAME = "route-change";
+
+export const initRouter = (onRoute) => {
+  window.addEventListener(ROUTE_CHANGE_EVENT_NAME, (event) => {
+    const { nextUrl } = event.detail;
+
+    if (nextUrl) {
+      history.pushState(null, null, nextUrl);
+      onRoute();
+    }
+  });
+};
+
+export const push = (nextUrl) => {
+  window.dispatchEvent(
+    new CustomEvent(ROUTE_CHANGE_EVENT_NAME, {
+      detail: {
+        nextUrl,
+      },
+    })
+  );
+};
