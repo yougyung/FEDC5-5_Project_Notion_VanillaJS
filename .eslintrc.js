559b41d149096b916cdc24dddbad2906aa2242d5
Jaewoong Hwang <noa9925@gmail.com>
2023-10-19 11:03:20 +0900

    Init: "API 및 로컬스토리지 통신 세팅"

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,8 @@ module.exports = {
     'operator-linebreak': ['error', 'before'],
     'no-unused-expressions': ['error', { allowTernary: true }],
     'import/extensions': ['error', 'ignorePackages'],
-    'no-param-reassign': ['error', { props: false }],
+    'no-param-reassign': ['error', { props: false }], // 속성 재할당을 제외한 변수 재할당 제한
+    'no-alert': 'off',
+    'consistent-return': 'off', // 일관된 타입을 반환하지 않아도 되도록 함
   },
 };
---
+++ src/util/api.js
@@ -0,0 +1,20 @@
+export const API_END_POINT = 'https://kdt-frontend.programmers.co.kr';
+const USERNAME = 'jaewoong';
+
+export const request = async (url, options = {}) => {
+  try {
+    const res = await fetch(`${API_END_POINT}${url}`, {
+      ...options,
+      headers: { 'Content-Type': 'application/json', 'x-username': USERNAME },
+      body: {},
+    });
+
+    if (res.ok) {
+      return await res.json();
+    }
+
+    throw new Error('API 호출 오류');
+  } catch (e) {
+    alert(e.message);
+  }
+};
