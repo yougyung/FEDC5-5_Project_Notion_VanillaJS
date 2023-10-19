227222fe029ad667de63fbc01fd76721aee5ca45
hellosonic-r <hellommmh@gmail.com>
2023-10-19 12:29:31 +0900

    Feat : 로컬 스토리지 데이터 관리 컴포넌트 생성

---
+++ src/Storage.js
@@ -0,0 +1,12 @@
+export function setItem(key, value) {
+    window.localStorage.setItem(key, JSON.stringify(value));
+}
+
+export function getItem(key, defaultValue = []) {
+    try {
+        const storedValue = window.localStorage.getItem(key);
+        return storedValue ? JSON.parse(storedValue) : defaultValue; 
+    } catch (e) {
+        console.log('로컬 스토리지에 저장된 데이터가 없습니다.');
+    }
+}
