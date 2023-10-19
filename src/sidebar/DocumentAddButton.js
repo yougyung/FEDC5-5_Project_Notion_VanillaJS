0d4cd3fe195592dcd58665f02e6cca7ec392d20b
mibu119 <horang296@gmail.com>
2023-10-19 12:24:53 +0900

    📝 🔥 🎨 addbutton 클릭 이벤트 변경, 로직 주석 표시

--- src/sidebar/DocumentAddButton.js
+++ src/sidebar/DocumentAddButton.js
@@ -1,8 +1,12 @@
-export default function DocumentAddButton({ $target, onAdd }) {
+import { push } from "../utils/router.js";
+
+export default function DocumentAddButton({ $target, initialState }) {
   const $documentAddButton = document.createElement("button");
   $documentAddButton.className = "add-button";
   $documentAddButton.type = "button";
   $target.appendChild($documentAddButton);
+
+  this.state = initialState;
 
   this.render = () => {
     $documentAddButton.innerHTML = `
@@ -10,9 +14,12 @@ export default function DocumentAddButton({ $target, onAdd }) {
     `;
   };
 
-  $documentAddButton.addEventListener("click", () => {
-    onAdd();
-  });
-
   this.render();
+
+  $documentAddButton.addEventListener("click", () => {
+    // editpage는 변경됨, 그러나 아직 documentlist에 새로운 document가 추가되지 않음.
+    push("new");
+
+    // documentlist의 state를 변경하면서 새로운 document를 추가해야 함.
+  });
 }
