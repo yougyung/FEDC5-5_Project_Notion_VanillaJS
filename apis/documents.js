79103478c82d2ccc84823cd602286316d1148bee
이종혁 <whdgur57117@gmail.com>
2023-10-19 09:48:31 +0900

    [Feat] document 조회 API 호출 함수 생성

---
+++ apis/documents.js
@@ -0,0 +1,21 @@
+const API_ENDPOINT = `https://kdt-frontend.programmers.co.kr`
+const headers = {
+  'x-username': 'whdgur5717',
+}
+export const requestDocumentInfo = async (url, option) => {
+  try {
+    const res = await fetch(`${API_ENDPOINT}/${url}`, {
+      headers,
+      ...option,
+    })
+    if (!res.ok) {
+      console.log(res.status, res.statusText)
+      throw new Error(res)
+    }
+    const data = await res.json()
+    return data
+  } catch (e) {
+    console.error(e.statusText)
+    throw new Error(e.statusText)
+  }
+}
