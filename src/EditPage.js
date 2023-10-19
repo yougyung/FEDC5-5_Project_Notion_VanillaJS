bc4e812946cc80659cc880e2d38f87a63c5625b4
hellosonic-r <hellommmh@gmail.com>
2023-10-19 23:33:12 +0900

    Feat : 제목, 내용 편집 컴포넌트 생성

---
+++ src/EditPage.js
@@ -0,0 +1,47 @@
+import Editor from "./Editor.js";
+import { request } from "./Api.js";
+import { setItem, getItem } from "./Storage.js";
+
+export default function EditPage({ $target, initialState, onNewTitle }) {
+  const $div = document.createElement("div");
+  $target.append($div);
+
+  this.state = initialState;
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    editor.setState(nextState);
+  };
+
+  let timer = null;
+
+  const editor = new Editor({
+    $target,
+    initialState: {
+      title: "제목을 입력해주세요.",
+      content: "내용을 입력해주세요.",
+    },
+    onEditing: async (nextState) => {
+      const { id } = nextState;
+      onNewTitle(id);
+    },
+    // onEditing : (nextState) => {
+    //     setItem('localsavepoint', nextState);
+
+    //     if (timer !== null) {
+    //         clearTimeout(timer);
+    //     }
+
+    //     timer = setTimeout(async (nextState) => {
+    //         request
+    //     },3000)
+
+    // }
+  });
+
+  this.render = () => {
+    $div.innerHTML = "edit 페이지";
+  };
+
+  this.render();
+}
---
+++ src/Editor.js
@@ -0,0 +1,41 @@
+import { setItem, getItem } from "./Storage.js";
+
+export default function Editor({ $target, initialState, onEditing }) {
+  const $div = document.createElement("div");
+  $target.appendChild($div);
+
+  this.state = initialState;
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    $div.querySelector("[name=title").value = this.state.title;
+    $div.querySelector("[name=content]").value = this.state.content;
+    setItem("savepoint", this.state);
+    this.render();
+  };
+
+  let isAlreadyRender = false;
+
+  this.render = () => {
+    if (!isAlreadyRender) {
+      $div.innerHTML = `
+            <input name="title" type="text" value="${this.state.title}"></input>
+            <textarea name="content" style="width: 400px; height: 500px">${this.state.content}</textarea>
+            `;
+      isAlreadyRender = true;
+    }
+  };
+
+  $div.addEventListener("keyup", (e) => {
+    const name = e.target.getAttribute("name");
+    const nextState = {
+      ...this.state,
+      [name]: e.target.value,
+    };
+    console.log(nextState);
+    this.setState(nextState);
+    onEditing(nextState);
+  });
+
+  this.render();
+}
