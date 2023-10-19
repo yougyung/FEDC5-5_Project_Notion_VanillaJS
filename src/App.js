54a4e4a7131687acdfc45aa18f0f71aa05cbc88c
mibu119 <horang296@gmail.com>
2023-10-19 11:40:58 +0900

    🎨 app.js에 documentEditPage 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import Sidebar from "./sidebar/Sidebar.js";
+import DocumentEditPage from "./textEditor/DocumentEditPage.js";
 
 export default function App({ $target }) {
   const sidebar = new Sidebar({
     $target,
     initialState: [],
+  });
+
+  const documentEditPage = new DocumentEditPage({
+    $target,
+    initialState: {
+      documentId: "new",
+      document: {
+        title: "",
+        content: "",
+      },
+    },
   });
 }
