3d18dc6cc7d733a63013a4f7e909530d58eb18f2
Baeg <73841260+bgyoons@users.noreply.github.com>
2023-10-20 13:49:20 +0900

    feat: document 컴포넌트 레이아웃 구성

--- src/components/Document.js
+++ src/components/Document.js
@@ -1,15 +1,32 @@
 export default function Document({ $container }) {
   const $document = document.createElement("div");
-  $document.className = "document";
-  $document.textContent = "document";
+  $document.id = "document";
   $container.appendChild($document);
 
-  this.state = null;
+  this.state = {
+    title: "hi",
+    content: "hello",
+  };
 
   this.setState = (nextState) => {
     this.state = nextState;
     this.render();
   };
 
-  this.render = () => {};
+  this.render = () => {
+    $document.innerHTML = `
+      <input type="text" name="title" value="${this.state.title}">
+      <textarea name="content">${this.state.content}</textarea>
+    `;
+  };
+
+  $document.addEventListener("input", (e) => {
+    const { name } = e.target;
+
+    if (name === "title") {
+    } else if (name === "content") {
+    }
+  });
+
+  this.render();
 }
