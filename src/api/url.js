64c4bc1558f3f8f269ccfb8021aafbdb9f59f7c3
Baeg <73841260+bgyoons@users.noreply.github.com>
2023-10-20 11:38:18 +0900

    refactor: api 메서드 별로 url 변수명 변경

--- src/api/url.js
+++ src/api/url.js
@@ -1,2 +1,4 @@
-export const API_DOCUMENT = "/documents";
-export const API_DOCUMENT_DETAIL = (id) => `/documents/${id}`;
+export const GET_API_DOCUMENT = "/documents";
+export const POST_API_DOCUMENT = "/documents";
+export const GET_API_DOCUMENT_DETAIL = (id) => `/documents/${id}`;
+export const DELETE_API_DOCUMENT_DETAIL = (id) => `/documents/${id}`;
