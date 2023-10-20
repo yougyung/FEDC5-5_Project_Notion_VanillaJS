9a2933a2c7466855b0c0bff76f0d041d5a9827f4
Baek <woonn97@gmail.com>
2023-10-20 11:36:53 +0900

    fix: DocumentList수정
    * <ul> 수정
    * Document 클릭 시 페이지로 이동

--- src/components/DocumentList.js
+++ src/components/DocumentList.js
@@ -1,37 +1,46 @@
-export default function DocumentList({$target, initialState}) {
-    const $documentlist = document.createElement('div');
-    $target.appendChild($documentlist);
+import { push } from "../utils/router.js";
 
-    this.state = initialState;
+export default function DocumentList({ $target, initialState }) {
+  const $documentlist = document.createElement("div");
+  $target.appendChild($documentlist);
 
-    this.setState = nextState => {
-        this.state = nextState;
-        this.render();
-    }
+  this.state = initialState;
 
-    const renderDocument = (nextDocument) => `
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+
+  const renderDocument = (nextDocument) => `
         <ul>
-            ${nextDocument.map(
-                ({id, title, documents}) => `
+            ${nextDocument
+              .map(
+                ({ id, title, documents }) => `
                 <li data-id= ${id} class="document-item">
                     ${title}
                     <button class="add" type="button">+</button>
                 </li>
-                ${documents.length ? renderDocument(documents) : 'No Pages'}`
-            )}
+                ${documents.length ? renderDocument(documents) : "No Pages"}`
+              )
+              .join("")}
+        </ul>
         `;
 
-    this.render = () => {
-        if(this.state.length > 0) {
-            $documentlist.innerHTML = renderDocument(this.state);
-        }
+  this.render = () => {
+    if (this.state.length > 0) {
+      $documentlist.innerHTML = renderDocument(this.state);
     }
+  };
 
-    $documentlist.addEventListener('click', (e) => {
-        const $li = e.target.closest('li');
-        const {id} = $li.dataset;
-        console.log(id);
-    })
+  $documentlist.addEventListener("click", (e) => {
+    const $li = e.target.closest("li");
+    const { id } = $li.dataset;
+    if (e.target.className === "document-item") {
+      push(`/documents/${id}`);
+    } else if (e.target.className === "add") {
+      push(`/documents/new`);
+    }
+  });
 
-    this.render();
+  this.render();
 }
