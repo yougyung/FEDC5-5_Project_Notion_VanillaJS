029f78c3e69fa4031f5cead636794483fdd34594
Kim-Jaemin420 <jany240@naver.com>
2023-10-20 13:12:19 +0900

    Feat: router 설정

--- src/App.ts
+++ src/App.ts
@@ -1,61 +1,23 @@
-import { createComponent, useState } from "./core";
-import { DocumentView, Editor, Sidebar } from "./components";
+import { createComponent } from "./core";
+import { Editor, Sidebar, UserGuide } from "./components";
 import styles from "./app.module.scss";
-
-const DUMMY_DOCUMENT = {
-  id: 1,
-  title: "노션을 만들자",
-  content: "즐거운 자바스크립트의 세계!",
-  documents: [
-    {
-      id: 2,
-      title: "",
-      createdAt: "",
-      updatedAt: "",
-    },
-  ],
-  createdAt: "",
-  updatedAt: "",
-};
-
-const DUMMY_DATA = [
-  {
-    id: 1,
-    title: "노션을 만들자",
-    documents: [
-      {
-        id: 2,
-        title: "블라블라",
-        documents: [
-          {
-            id: 3,
-            title: "함냐함냐",
-            documents: [],
-          },
-        ],
-      },
-    ],
-  },
-  {
-    id: 4,
-    title: "hello!",
-    documents: [],
-  },
-];
 
 const { s_container, s_mainContainer } = styles;
 
 function App() {
-  const [activeView, setActiveView] = useState<"editor" | "documentView" | null>("editor");
-  const [documents, setDocuments] = useState(DUMMY_DATA);
+  function matchRoute(path: string) {
+    switch (path) {
+      case "/":
+        return createComponent(UserGuide);
+      case /^\/documents\/(?<documentId>\d+)$/.test(path) ? path : null:
+        return createComponent(Editor, { documentId: Number(path.match(/\/documents\/(\d+)/)?.[1]) ?? null });
+      default:
+        return createComponent(UserGuide);
+    }
+  }
 
-  const addRootDocument = () => {
-    setActiveView("editor");
-  };
-
-  const sidebarComponent = createComponent(Sidebar, { documents, addRootDocument });
-  const editorComponent = createComponent(Editor);
-  const documentViewComponent = createComponent(DocumentView, { document: DUMMY_DOCUMENT });
+  const sidebarComponent = createComponent(Sidebar, { documents, createDocument });
+  const mainComponent = matchRoute(window.location.pathname);
 
   const bindEvents = () => {
     sidebarComponent.bindEvents?.();
@@ -69,8 +31,7 @@ function App() {
         </h1>
         ${sidebarComponent.element}
         <main class=${s_mainContainer}>
-          ${activeView === "editor" ? editorComponent.element : ""}
-          ${activeView === "documentView" ? documentViewComponent.element : ""}
+          ${mainComponent.element}
         </main>
       </div>
     `,
