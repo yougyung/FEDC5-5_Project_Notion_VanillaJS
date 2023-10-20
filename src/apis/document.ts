aac0ce697e96cdf07241d24e50c210b23a772d11
Kim-Jaemin420 <jany240@naver.com>
2023-10-20 18:34:43 +0900

    Feat: 문서 post 하는 api 함수 구현

--- src/apis/document.ts
+++ src/apis/document.ts
@@ -2,9 +2,19 @@ import api from "./core";
 
 export const getDocuments = async () => {
   try {
-    const documents = await api.get("/documents");
+    const documents = await api.get({ url: "/documents" });
 
     return await documents;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const postDocument = async ({ title, parent }: { title: string; parent: null | number }) => {
+  try {
+    const postedDocument = await api.post({ url: "/documents", body: { title, parent } });
+
+    return await postedDocument;
   } catch (error) {
     console.error(error);
   }
