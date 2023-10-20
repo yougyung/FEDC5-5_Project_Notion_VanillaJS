10e7cfe08ac90b0065ff9ce8a21001eb6a57737a
kimyou1102 <yoosk5485@naver.com>
2023-10-20 10:56:11 +0900

    feat: route 커스텀 이벤트 생성

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import DocumentList from "./DocumentList.js";
 import DocumentEditSection from "./DocumentEditSection.js";
 import { request } from "../utils/api.js";
+import { initRouter, push } from "../utils/route.js";
 
 export default function App({ $target }) {
   const documentList = new DocumentList({
     $target,
     initialState: [],
+    onDocumentItemClick: (id) => {
+      push(`/document/${id}`);
+    },
   });
 
   const $editorSection = document.createElement("div");
@@ -25,12 +29,11 @@ export default function App({ $target }) {
 
   this.route = () => {
     const { pathname } = window.location;
-
+    $editorSection.innerHTML = "";
     if (pathname === "/") {
       $editorSection.innerHTML = "<h1>아직 문서를 선택하지 않았습니다.</h1>";
     } else if (pathname.indexOf("/document/") === 0) {
       const [, , documentId] = pathname.split("/");
-
       documentEditSection.setState({
         documentId,
         document: {
@@ -49,4 +52,6 @@ export default function App({ $target }) {
   };
 
   fetchDocments();
+
+  initRouter(() => this.route());
 }
