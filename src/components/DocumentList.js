a95ec6448f38ad75d5026be3a0e5fc6f79d3fcec
yeonkyoung <keep.ykk@gmail.com>
2023-10-21 03:01:07 +0900

    feat: 데이터를 순회하며 title list 생성

--- src/components/DocumentList.js
+++ src/components/DocumentList.js
@@ -1,14 +1,39 @@
 export default function DocumentList({ $target, initialState }) {
 	const $nav = document.createElement('nav');
+	const $ul = document.createElement('ul');
 
 	$target.appendChild($nav);
 
 	this.state = initialState;
+
 	this.setState = (nextState) => {
 		this.state = nextState;
 		this.render();
 	};
+
+	this.stateRecursion = (documentsArray, $parentElement) => {
+		const $ul = document.createElement('ul');
+		documentsArray.sort((a, b) => {
+			a.createdAt - b.createdAt;
+		});
+
+		documentsArray.forEach(({ id, title, documents }) => {
+			const $li = document.createElement('li');
+
+			$li.setAttribute('data-id', id);
+			$li.innerHTML = `
+			<span>${title ? title : EMPTY_TITLE}<span>
+			`;
+
+			if (documents.length) this.stateRecursion(documents, $li);
+			$ul.appendChild($li);
+		});
+		$parentElement.appendChild($ul);
+	};
+
 	this.render = () => {
+		this.stateRecursion(this.state, $ul);
+		$nav.appendChild($ul);
 	};
 	this.render();
 }
