7a084a549bd808f94dcff0aa943ea60f0aedb27a
Kim-Jaemin420 <jany240@naver.com>
2023-10-20 16:50:51 +0900

    Feat: 컴포넌트 export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
-export { default as Document } from "./Document";
+export { default as DocumentItem } from "./DocumentItem/DocumentItem";
 export { default as Sidebar } from "./Sidebar/Sidebar";
+export { default as Editor } from "./Editor/Editor";
+export { default as UserGuide } from "./UserGuide/UserGuide";
+export { default as NotFound } from "./NotFound/NotFound";
