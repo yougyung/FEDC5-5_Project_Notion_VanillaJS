278d6a535219aab6bf97448a9513bb4d885aacd8
mibu119 <horang296@gmail.com>
2023-10-20 12:49:56 +0900

    ✨ 현재 편집 중인 document의 하위 documents 링크 렌더링

--- src/components/documentComponents/SubDocumentList.js
+++ src/components/documentComponents/SubDocumentList.js
@@ -1,3 +1,5 @@
+import { push } from "../../utils.js";
+
 export default function SubDocumentList({ $target, initialState }) {
   const $editorFooter = document.createElement("nav");
   $editorFooter.className = "editor-bottom-nav";
@@ -11,18 +13,34 @@ export default function SubDocumentList({ $target, initialState }) {
   };
 
   this.render = () => {
+    const { documents } = this.state;
     $editorFooter.innerHTML = `
-      <ul class="subdocument-list">
-        ${this.state.documents.map(
-          ({ id, title }) => `
-          <li data-id=${id}>
-            ${(title ?? "제목 없음") || (title === "" && "제목 없음")}
-          </li>
-          `
-        )}
-      </ul>
+    <ul class="subdocument-list">
+      ${documents.map(
+        ({ id, title }) => `
+        <li data-id=${id} class="subdocument-link">
+          ${(title ?? "제목 없음") || (title === "" && "제목 없음")}
+        </li>
+        `
+      )}
+    </ul>
         `;
   };
+
+  $editorFooter.addEventListener("click", (event) => {
+    event.stopPropagation();
+
+    const { target } = event;
+    const $li = target.closest("li");
+
+    let { id } = $li.dataset;
+    id = parseInt(id);
+
+    if (!isNaN(id)) {
+      console.log(id);
+      push(`${id}`);
+    }
+  });
 
   this.render();
 }
