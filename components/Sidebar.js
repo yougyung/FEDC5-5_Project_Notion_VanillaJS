1de1b2439c58a64e5b0b867585c8de39dabef1ff
이종혁 <whdgur57117@gmail.com>
2023-10-22 01:32:05 +0900

    [Refactor] 렌더링 방식을 innerHTML으로 변경 및 폴더 구조 생성 로직 변경

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,61 +2,36 @@ import { requestDocument, HTTPError } from '../apis/documents.js'
 export default class Sidebar {
   constructor({ $target }) {
     this.$target = $target
+    this.state = []
     this.setup()
     this.fetchDirectory()
     this.setEvent()
   }
   setup() {
-    //sidebar 안에 div들이 들어가야 함
-    this.$sidebar = document.createElement('div')
-    this.$sidebar.id = 'sidebar'
-    this.$directory = document.createElement('div')
-    this.$directory.id = 'directory'
-    this.$addDocumentNode = document.createElement('div')
-    this.$addDocumentNode.innerText = '페이지 추가'
-    this.$sidebar.appendChild(this.$directory)
-    this.$sidebar.appendChild(this.$addDocumentNode)
-    this.$target.appendChild(this.$sidebar)
-  }
-  renderTreeByDiv(data, parentElement) {
-    for (const item of data) {
-      const $document = document.createElement('div')
-      const $link = document.createElement('a')
-      const $span = document.createElement('span')
-      const $button = document.createElement('button')
-      $span.textContent = item.title
-      $button.textContent = '➕'
-      $link.appendChild($span)
-      $document.append($link, $button)
-      $button.addEventListener('click', (e) => {
-        e.stopPropagation()
-        const { id } = e.target.parentNode.dataset
-        this.createNewDocument(Number(id))
-        //해당 id값으로 post요청하면 -> 생성된 객체 전달
-        //찾은 객체의 documents에 응답받은 데이터 넣으면 됨
-      })
-      $document.dataset.id = item.id
-      $document.classList.add('document')
-      parentElement.appendChild($document)
-      if (item.documents.length) {
-        this.renderTreeByDiv(item.documents, $document)
-      }
-    }
+    this.$target.innerHTML = `<div id=sidebar>
+      <div id=directory></div>
+      <div role='button' id='add'></div>
+      </div>`
+    this.$sidebar = document.querySelector('#sidebar')
+    this.$directory = document.querySelector('#directory')
+    this.$add = document.querySelector('#add')
   }
 
-  renderTreeByList(data, parentElement) {
-    const $ul = document.createElement('ul')
-    for (const item of data) {
-      const $li = document.createElement('li')
-      const $span = document.createElement('span')
-      $span.innerText = item.title
-      $li.appendChild($span)
-      if (item.documents.length) {
-        this.renderTreeByList(item.documents, $li)
-      }
-      $ul.appendChild($li)
-    }
-    parentElement.appendChild($ul)
+  renderTreeByDiv() {
+    return this.state
+      .map((document) => this.createSbInnerHTML(document))
+      .join('')
+  }
+
+  createSbInnerHTML(item) {
+    return `
+    <div>
+    <div class="title" data-id=${item.id}>${item.title}</div>
+        <div class="sub">${item.documents
+          .map((data) => this.createSbInnerHTML(data))
+          .join('')}</div>
+    </div>
+      `
   }
 
   createNewDocument(parentId) {
@@ -71,31 +46,13 @@ export default class Sidebar {
     //추가했으면 -> 데이터 새로 요청할수도 or 현재 state에 껴넣을수도
   }
 
-  findNodeById(data, targetId) {
-    for (const item of data) {
-      if (item.id === targetId) {
-        console.log(item)
-        return item
-      }
-      if (item.documents.length) {
-        this.findNodeById(item.documents, targetId)
-      }
-    }
-  }
-
   setState(nextState) {
     this.state = nextState
     this.render()
   }
 
   render() {
-    const $documentList = document.createElement('div')
-    $documentList.id = 'document_list'
-    this.renderTreeByDiv(this.state, $documentList)
-    !this.prevChild
-      ? this.$directory.appendChild($documentList)
-      : this.$directory.replaceChild($documentList, this.prevChild)
-    this.prevChild = $documentList
+    document.querySelector('#directory').innerHTML = this.renderTreeByDiv()
   }
   async fetchDirectory() {
     try {
@@ -104,6 +61,8 @@ export default class Sidebar {
     } catch (err) {
       if (err instanceof HTTPError) {
         err.showAlert
+      } else {
+        console.error(err)
       }
     }
   }
