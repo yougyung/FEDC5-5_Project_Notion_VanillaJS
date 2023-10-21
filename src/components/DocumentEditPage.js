0f9c1ee703bc881f10c224fdddd0f4790a2911d0
yeonkyoung <keep.ykk@gmail.com>
2023-10-22 00:49:56 +0900

    feat: DocumentEditPage 초기 화면 설정

---
+++ src/components/DocumentEditPage.js
@@ -0,0 +1,34 @@
+export default function DocumentEditPage({
+	$target,
+	initialState,
+}) {
+	const $div = document.createElement('div');
+
+	$div.setAttribute('class', 'edit_form');
+	$target.appendChild($div);
+
+	this.state = initialState;
+
+	$div.innerHTML = `<input name='title' placeholder=''></input><textarea name='content'></textarea>`;
+
+	this.setState = (nextState) => {
+		this.state = nextState;
+		this.render();
+	};
+
+	const $input = document.querySelector(`input[name='title']`);
+	const $textarea = document.querySelector(`textarea[name='content']`);
+
+	this.render = () => {
+		const { id, title, content, documents, createdAt, updatedAt } =
+			this.state;
+
+		if (title) {
+			$input.value = title;
+		} else {
+			$input.placeholder = EMPTY_TITLE;
+		}
+		$textarea.value = content ? content : '';
+	};
+	this.render();
+}
