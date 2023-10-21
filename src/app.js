9524f927118543720d616aa5b33c0e536a9b73b5
YunSukHyun <czcz9207@gmail.com>
2023-10-21 16:31:40 +0900

    feat: show sidemenu

--- src/app.js
+++ src/app.js
@@ -1,7 +1,66 @@
 import Posting from "./components/Posting.js";
 import SideMenu from "./components/SideMenu.js";
+import { request } from "./utils/api.js";
 
 export default function App({ $target }) {
-  new SideMenu({ $target });
-  new Posting({ $target });
+  const $sideMenuContainer = document.createElement("div");
+  const $postsContainer = document.createElement("div");
+
+  this.state = {
+    documentList: [],
+    selectedDocument: null,
+    isDocumentsLoading: false,
+  };
+
+  $target.appendChild($sideMenuContainer);
+  $target.appendChild($postsContainer);
+
+  const sideMenu = new SideMenu({
+    $target: $sideMenuContainer,
+    initialState: this.state.documentList,
+    onSelect: async (document) => {
+      history.pushState(null, null, `?selectedDocument=${document}`);
+      this.setState({
+        ...this.state,
+        selectedDocument: document,
+      });
+      await fetchSelectedDocument();
+    },
+  });
+
+  const posting = new Posting({
+    $target: $postsContainer,
+    initialState: this.state.selectedDocument,
+  });
+
+  this.setState = (nextstate) => {
+    this.state = nextstate;
+  };
+
+  const fetchDocumentList = async () => {
+    const documentList = await request();
+    this.setState({
+      ...this.state,
+      documentList,
+    });
+    sideMenu.setState(documentList);
+    // console.log(documentList);
+    // console.log(this.state);
+  };
+
+  const fetchSelectedDocument = async () => {
+    const { selectedDocument } = this.state;
+    if (selectedDocument) {
+      this.setState({ ...this.state, isDocumentsLoading: true });
+      const currentDocument = await request(selectedDocument);
+      this.setState({
+        ...this.state,
+        selectedDocument: currentDocument,
+        isDocumentsLoading: false,
+      });
+    }
+  };
+
+  fetchDocumentList();
+  fetchSelectedDocument();
 }
--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,1 +1,26 @@
-export default function SideMenu($target) {}
+export default function SideMenu({ $target, initialState, onSelect }) {
+  const $nav = document.createElement("nav");
+  $target.appendChild($nav);
+  this.state = initialState;
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+
+  this.render = () => {
+    $nav.innerHTML = `<h1>Documents</h1><ul>${this.state
+      .map((document) => `<li data-id="${document.id}">${document.title}</li>`)
+      .join(
+        ""
+      )}<li><form><input class="new-document" type="text" placeholder="Add document"></form></li></ul>`;
+  };
+
+  $nav.addEventListener("click", (e) => {
+    const $li = e.target.closest("li[data-id]");
+    if ($li) {
+      const { id } = $li.dataset;
+      onSelect(id);
+    }
+  });
+}
