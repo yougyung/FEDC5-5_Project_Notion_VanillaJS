7bcafa75de1ab9a425980aa9e9f8b0590105ae08
yeonkyoung <keep.ykk@gmail.com>
2023-10-22 00:44:25 +0900

    feat: 목록의 제목 클릭 콜백, 새 페이지 생성 클릭 콜백 추가

--- src/App.js
+++ src/App.js
@@ -2,8 +2,25 @@ import DocumentList from './components/DocumentList.js';
 
 export default function App({ $target, initialState }) {
 	this.state = initialState;
-	const documentList = new DocumentList({ $target, initialState });
 
+	const documentList = new DocumentList({
+		$target,
+		initialState,
+		onTitleClick: (id) => {
+		},
+		onCreatePage: () => {
+			const date = new Date();
+			const newState = {
+				id: date.getTime().toString(),
+				title: '',
+				documents: [],
+				createdAt: date.toJSON(),
+				updatedAt: date.toJSON(),
+			};
+
+			documentEditPage.setState(nextState);
+		},
+	});
 	this.setState = (nextState) => {
 		this.state = nextState;
 		documentList.setState(nextState);
