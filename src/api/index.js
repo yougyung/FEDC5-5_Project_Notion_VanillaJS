b1879d00a58e470a9891dc48de457543b82d7804
seongminn <berry9901@gmail.com>
2023-10-22 15:57:57 +0900

    fix: `max-lines-per-function` 옵션에 따라 api request 함수 수정

--- src/api/index.js
+++ src/api/index.js
@@ -1,21 +1,31 @@
 const { BASE_URL, USER_NAME } = process.env;
 
+const buildHeaders = (customHeaders = {}) => {
+  return {
+    'Content-Type': 'application/json',
+    'x-username': USER_NAME,
+    ...customHeaders,
+  };
+};
+
+const handleResponse = async (res) => {
+  if (!res.ok) {
+    throw new Error('데이터가 도망쳤어요!');
+  }
+
+  return res.json();
+};
+
 export const request = async (pathname, options = {}, payload = null) => {
   try {
-    const res = await fetch(`${BASE_URL}${pathname}`, {
-      headers: {
-        'Content-Type': 'application/json',
-        'x-username': USER_NAME,
-        ...options.headers,
-      },
-      ...options,
-      body: payload ? JSON.stringify(payload) : null,
-    });
+    const headers = buildHeaders(options.headers);
+    const url = `${BASE_URL}${pathname}`;
+    const body = payload ? JSON.stringify(payload) : null;
 
-    if (!res.ok) throw new Error('데이터가 도망쳤어요!');
+    const res = await fetch(url, { ...options, headers, body });
 
-    return res.json();
+    return await handleResponse(res);
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 };
