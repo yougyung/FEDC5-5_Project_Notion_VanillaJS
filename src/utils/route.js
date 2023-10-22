20d6c67eb10d8e970a21ceeb78b0d39e8fbe50f2
kimyou1102 <yoosk5485@naver.com>
2023-10-22 17:41:52 +0900

    fix: 현재 문서로 또 이동 시 편집기가 비게되는 문제 해결

--- src/utils/route.js
+++ src/utils/route.js
@@ -3,9 +3,10 @@ const ROUTE_CHANGE_EVENT_NAME = "route-change";
 export const initRouter = (onRoute) => {
   window.addEventListener(ROUTE_CHANGE_EVENT_NAME, (e) => {
     const { nextUrl } = e.detail;
+    const { pathname } = window.location;
     const state = e.detail.state ? e.detail.state : null;
 
-    if (nextUrl) {
+    if (nextUrl && nextUrl !== pathname) {
       history.pushState(state, null, nextUrl);
       onRoute();
     }
