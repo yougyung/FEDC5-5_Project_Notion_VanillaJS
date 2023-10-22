fd3b76cc070e43291ddb90ff30c92177e80e0bb7
이종혁 <whdgur57117@gmail.com>
2023-10-22 23:34:13 +0900

    [Init] eslint,prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  rules: {
+    "import/prefer-default-export": "off",
+    "import/extensions": ["off"],
+    "class-methods-use-this": "warn",
+  },
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+}
