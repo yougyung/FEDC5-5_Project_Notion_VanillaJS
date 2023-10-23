4890def3b96de0bc73e3fbb7a4f3b5fbeeebeb60
Kim-Jaemin420 <jany240@naver.com>
2023-10-23 22:57:28 +0900

    Feat: 기존 파일명에서 Dto를 붙여 api 관련 타입인 것을 명시
    request, response에 대한 타입들을 명시하고 끝에 Dto 접미사를
    붙여 api와 관련된 타입인 것을 명시함.

---
+++ src/types/documentDto.ts
@@ -0,0 +1,38 @@
+export interface DocumentResponseDto {
+  id: number;
+  title: string;
+  documents: DocumentResponseDto[];
+}
+
+export interface DocumentDetailResponseDto {
+  id: number;
+  title: string;
+  content: string;
+  documents: DocumentMetaDto[];
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface DocumentMetaDto {
+  id: number;
+  title: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface DocumentPostRequestDto {
+  title: string;
+  parent: number | null;
+}
+
+export interface DocumentPostResponseDto {
+  id: number;
+  title: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface DocumentPutRequestDto {
+  title: string;
+  content: string;
+}
