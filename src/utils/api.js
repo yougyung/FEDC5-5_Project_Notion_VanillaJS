15c2b5613b3cedc102d7cbf2a215065fc2eacc62
SoJuSo <asdfg7123@gmail.com>
2023-10-23 09:30:57 +0900

    [Refactor] 더미데이터 삭제

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,58 +1,5 @@
 import { DEFAULT_USERNAME, API_END_POINT } from "../../env.js";
 
-// 일단 더미데이터로 실험해보고 이후에 API 연결하기
-export const DUMMY_DATA_SIDE_LIST = [
-  {
-    id: 1, // Document id
-    title: "노션을 만들자", // Document title
-    documents: [
-      {
-        id: 2,
-        title: "블라블라",
-        documents: [
-          {
-            id: 3,
-            title: "함냐함냐",
-            documents: [],
-          },
-          {
-            id: 100,
-            title: "hello!",
-            documents: [],
-          },
-        ],
-      },
-      { id: 4, title: "함블함블", documents: [] },
-    ],
-  },
-  {
-    id: 5,
-    title: "hello!",
-    documents: [],
-  },
-  {
-    id: 6,
-    title: "hello!2",
-    documents: [],
-  },
-];
-
-export const DUMMY_DATA_TEXT_CONTENT = {
-  id: 1,
-  title: "노션을 만들자",
-  content: "즐거운 자바스크립트의 세계!",
-  documents: [
-    {
-      id: 2,
-      title: "",
-      createdAt: "",
-      updatedAt: "",
-    },
-  ],
-  createdAt: "",
-  updatedAt: "",
-};
-
 export const request = async (url, options = {}) => {
   try {
     const res = await fetch(`${API_END_POINT}${url}`, {
