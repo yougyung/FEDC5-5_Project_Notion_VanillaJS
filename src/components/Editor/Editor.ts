b2c81d7f1b2dd01b90fd1c2b4a7c618e57bafd64
Kim-Jaemin420 <jany240@naver.com>
2023-10-23 23:51:11 +0900

    Feat: form에 event handler 바인딩

--- src/components/Editor/Editor.ts
+++ src/components/Editor/Editor.ts
@@ -2,19 +2,35 @@ import styles from "./editor.module.scss";
 
 const { s_editorForm, s_editorInput, s_editorContent } = styles;
 
+const handleKeydownForm = (event: Event) => {
+  event.preventDefault();
+
+  const $title = window.document.querySelector("#title") as HTMLInputElement;
+  const $content = window.document.querySelector("#content") as HTMLTextAreaElement;
+};
+
 function Editor() {
+  const bindEvents = () => {
+    const $form = window.document.querySelector(`.${s_editorForm}`) as HTMLFormElement;
+
+    $form.addEventListener("keyup", handleKeydownForm);
+  };
+
   return {
     element: `
       <form class=${s_editorForm}>
         <fieldset>
           <legend class="a11yHidden">새 문서 작성</legend>
           <label for="title">제목</label>
-          <input id="title" type="text" class=${s_editorInput}/>
+          <input id="title" type="text" class="${s_editorInput}" required/>
           <label for="content">내용</label>
-          <textarea id="content" rows=50 class=${s_editorContent}></textarea>
+          <textarea id="content" rows=50 class="${s_editorContent}" >
+          </textarea>
         </fieldset>
       </form>
+           
     `,
+    bindEvents,
   };
 }
 
