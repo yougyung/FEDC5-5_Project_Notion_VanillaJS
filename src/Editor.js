d6d2bd4e0a70de2c5a67aa4d6d1ce51496b3b3c6
lunarmoon7 <49crehbgr@gmail.com>
2023-10-23 10:52:47 +0900

    feat: rich editor로 변경 - textarea -> contenteditable

--- src/Editor.js
+++ src/Editor.js
@@ -1,5 +1,6 @@
 export default function Editor({ $target, initialState, onEditing }) {
   const $editor = document.createElement('div');
+  $editor.className = 'editor';
   $target.appendChild($editor);
 
   this.state = initialState;
@@ -17,29 +18,73 @@ export default function Editor({ $target, initialState, onEditing }) {
     this.render();
   };
 
-  $editor.innerHTML = `
-    <input type="text" name="title" style="width:500px;"/>
-    <textarea name="content" style="width:500px;height:400px;"></textarea>
-  `
+  this.renderRichContent = (content) => {
+    if (!content) return content;
 
-  this.render = () => {
-    $editor.querySelector('input[name=title]').value = this.state.title;
-    $editor.querySelector('textarea[name=content]').value = this.state.content;
+    const richContent = content
+      .split('<div>')
+      .map((line) => {
+        line = line.replace('</div>', '');
+        if (line === '') return '';
+        if (line.indexOf('# ') === 0) {
+          line = line.replace(/[\#]{1}(.+)/g, '<div><h1>$1</h1></div>');
+          console.log(line);
+        } else if (line.indexOf('## ') === 0) {
+          line = line.replace(/[\#]{2}(.+)/g, '<div><h2>$1</h2></div>');
+        } else if (line.indexOf('### ') === 0) {
+          line = line.replace(/[\#]{3}(.+)/g, '<div><h3>$1</h3></div>');
+        } else {
+          line = `<div>${line}</div>`;
+        }
+        return line;
+      })
+      .join('');
+
+    return richContent;
   };
 
-  $editor.addEventListener('keyup', (e) => {
-    const { target } = e;
+  $editor.innerHTML = `
+    <input class='edit-title' type="text" name="title" placeholder='제목을 입력하세요'/>
+    <div class='edit-main' name="content" contentEditable='true' placeholder='내용을 입력하세요.'></div>
+  `;
 
-    const name = target.getAttribute('name');
+  this.render = () => {
+    $editor.querySelector('[name=title]').value = this.state.title;
 
-    if (this.state[name] !== undefined) {
-      const nextState = {
-        ...this.state,
-        [name]: target.value,
-      };
-      this.setState(nextState);
-      onEditing(this.state);
-    }
+    const $content = $editor.querySelector('[name=content]');
+    const richContent = this.renderRichContent(this.state.content);
+
+    $content.innerHTML = richContent;
+  };
+
+  $editor.querySelector('[name=title]').addEventListener('keyup', (e) => {
+    const { value } = e.target;
+
+    const nextState = {
+      ...this.state,
+      title: value,
+    };
+
+    this.setState(nextState);
+    onEditing(this.state);
+  });
+
+  $editor.querySelector('[name=content]').addEventListener('input', (e) => {
+    const nextState = {
+      ...this.state,
+      content: e.target.innerHTML,
+    };
+
+    this.setState(nextState);
+    onEditing(this.state);
+
+    const target = e.target;
+    const range = document.createRange();
+    const sel = window.getSelection();
+    range.selectNodeContents(target);
+    range.collapse(false); // move to the end of the range
+    sel.removeAllRanges();
+    sel.addRange(range);
+    target.focus();
   });
 }
-
