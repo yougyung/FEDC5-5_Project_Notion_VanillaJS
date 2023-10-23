124e95f9d2e93f90cf9f2feb07564e7619d7cb7c
lunarmoon7 <49crehbgr@gmail.com>
2023-10-23 12:48:17 +0900

    feat: headers 추가

--- src/api.js
+++ src/api.js
@@ -1,11 +1,12 @@
 export const API_END_POINT = 'https://kdt-frontend.programmers.co.kr/documents';
 
-export const request = async (url='', options = {}) => {
+export const request = async (url = '', options = {}) => {
   try {
     const response = await fetch(`${API_END_POINT}${url}`, {
       ...options,
       headers: {
         'x-username': 'zentechie',
+        'Content-Type': 'application/json',
       },
     });
 
