77a871d48f38408bc6e92e65140b41601a61b094
seongminn <berry9901@gmail.com>
2023-10-24 00:17:19 +0900

    refactor: main page 내부 구조 수정

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,20 +1,41 @@
-import { getAllDocuments, getDetailOfDocument } from '@/api/document';
-import Editor from '@/components/Editor';
-import Sidebar from '@/components/Sidebar';
+import { push } from '@/router';
 import Component from '@/core/Component';
-import { appendNewElementToParent } from '@/utils/dom';
-import { push } from '../router';
-import { API_END_POINT } from '../constants/api';
+import DocumentList from '@/components/DocumentList';
+import Editor from '@/components/Editor';
+
+import {
+  getAllDocuments,
+  createDocument,
+  deleteDocument,
+  getDetailOfDocument,
+  updateDocument,
+} from '@/api/document';
+import { API_END_POINT } from '@/constants/api';
 
 export default class MainPage extends Component {
-  constructor(args) {
-    super(args);
+  constructor($target) {
+    super($target);
 
     this.fetchDocumentList();
   }
 
+  // eslint-disable-next-line max-lines-per-function
   async setup() {
     this.state = { currentId: null };
+
+    this.$sidebar = document.createElement('aside');
+    this.$section = document.createElement('section');
+    this.$documentList = new DocumentList(this.$sidebar, {
+      onSelect: this.handleDocumentSelect.bind(this),
+      onCreate: this.handleDocumentCreate.bind(this),
+      onDelete: this.handleDocumentDelete.bind(this),
+    });
+    this.$editor = new Editor(this.$section, {
+      onEdit: this.handleEditorEdit.bind(this),
+    });
+
+    this.$target.appendChild(this.$sidebar);
+    this.$target.appendChild(this.$section);
   }
 
   setState(nextState) {
@@ -24,38 +45,55 @@ export default class MainPage extends Component {
     this.render();
   }
 
-  attachToTarget() {
-    this.$editorPage = appendNewElementToParent('section', this.$target);
-  }
+  render() {
+    if (!this.state.currentId) {
+      this.$editor.setState(null);
+      return;
+    }
 
-  addChildElements() {
-    this.$sidebar = new Sidebar(this.$editorPage, {
-      onSelect: (documentId) => push(`${API_END_POINT.DOCUMENTS}/${documentId}`),
-      onCreate: () => {},
-      onToggle: () => {},
-      onDelete: () => {},
-    });
-
-    this.$editor = new Editor(this.$editorPage, {
-      onEdit: () => {},
-    });
+    const currentDocument = this.fetchCurrentDocument(this.state.currentId);
+    this.$editor.setState(currentDocument);
   }
 
   async fetchDocumentList() {
     const documentList = await getAllDocuments();
 
-    this.$sidebar.setState({ ...this.$sidebar.state, documentList });
+    this.$documentList.setState({ ...this.$documentList.state, documentList });
   }
 
   async fetchCurrentDocument(documentId) {
-    const currentDocument = await getDetailOfDocument(documentId);
+    try {
+      const currentDocument = await getDetailOfDocument(documentId);
 
-    this.$editor.setState({ ...this.$editor.state, currentDocument });
+      this.$editor.setState(currentDocument);
+    } catch (error) {
+      // TODO 에러 발생 시 $editor.setState로 내부 값 변경해서 렌더링해주기
+      console.error(error.message);
+    }
   }
 
-  render() {
-    if (!this.state.currentId) return;
+  handleDocumentSelect(documentId) {
+    push(`${API_END_POINT.DOCUMENTS}/${documentId}`);
+  }
 
-    this.fetchCurrentDocument(this.state.currentId);
+  async handleDocumentCreate(documentId = null) {
+    const newDocument = await createDocument(documentId);
+
+    this.fetchDocumentList();
+    push(`${API_END_POINT.DOCUMENTS}/${newDocument.id}`);
+  }
+
+  async handleDocumentDelete(documentId) {
+    await deleteDocument(documentId);
+
+    // TODO push가 아니라, replace로 수정해야 함!
+    if (this.state.currentId === documentId) push('/');
+    this.fetchDocumentList();
+  }
+
+  async handleEditorEdit(nextState) {
+    const { id, title, content } = nextState;
+
+    await updateDocument(id, { title, content });
   }
 }
