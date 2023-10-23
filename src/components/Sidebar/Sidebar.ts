4d42ef734bc11e095cdc1d20e5a5a9fb239bebde
Kim-Jaemin420 <jany240@naver.com>
2023-10-24 00:57:17 +0900

    Modify: event handler binding 정리
    기존에 ul에 event delegation을 사용하여 바인딩한 이벤트
    핸들러 함수 내부에서 너무 다양한 일을 하고 있었음. 여러 가지
    돔을 클릭한 상황에 대한 분기처리가 많아 읽기 어려워져 분리

--- src/components/Sidebar/Sidebar.ts
+++ src/components/Sidebar/Sidebar.ts
@@ -1,5 +1,6 @@
 import { createComponent } from "@/core";
 import { DocumentItem } from "@/components";
+import { navigateTo } from "@/utils";
 import { Document } from "@/types";
 import styles from "./sidebar.module.scss";
 
@@ -8,47 +9,72 @@ const { s_sidebar, s_button } = styles;
 interface SidebarProps {
   documents: Document[];
   createDocument: ({ title, parent }: { title: string; parent: null | number }) => void;
+  removeDocument: (id: number) => void;
 }
 
-function Sidebar({ documents, createDocument }: SidebarProps) {
-  const addDocument = (event: Event) => {
-    const target = event.target as HTMLElement;
+function Sidebar({ documents, createDocument, removeDocument }: SidebarProps) {
+  const handleClickAddDocumentButton = (target: HTMLElement) => {
+    const id = Number(target.parentElement?.dataset.id);
 
-    if (target.classList.contains("add-document-button")) {
-      if (target.dataset && target.dataset.parentId === "null") {
-        createDocument({ title: "Untitled", parent: null });
-        return;
-      }
-      if (target.dataset && !isNaN(Number(target.dataset.parentId))) {
-        const parentId = Number(target.dataset.parentId);
-        createDocument({ title: "", parent: parentId });
+    createDocument({ title: "Untitled", parent: id });
+  };
 
-        return;
-      }
-      console.error("Invalid parent id:", target.dataset.parentId);
+  const handleClickDeleteButton = (target: HTMLElement) => {
+    const id = Number(target.parentElement?.dataset.id);
+
+    removeDocument(id);
+  };
+
+  const handleClickAddRootDocumentButton = (event: MouseEvent) => {
+    const target = event.target as HTMLButtonElement;
+
+    if (target.dataset && target.dataset.parentId === "null") {
+      createDocument({ title: "Untitled", parent: null });
     }
   };
-  const bindEvents = () => {
-    const $sidebar = document.querySelector(`.${s_sidebar}`) as HTMLElement;
 
-    $sidebar.addEventListener("click", addDocument);
+  const handleClickDocumentItem = (event: Event) => {
+    const target = event.target as HTMLElement;
+
+    if (target.parentElement && target.parentElement.dataset.id) {
+      const id = target?.parentElement.dataset.id;
+
+      navigateTo(`/documents/${id}`);
+    }
+
+    if (target.dataset && !isNaN(Number(target.dataset.parentId))) {
+      if (target.classList.contains("addDocumentButton")) {
+        handleClickAddDocumentButton(target);
+      }
+
+      if (target.classList.contains("deleteDocumentButton")) {
+        handleClickDeleteButton(target);
+      }
+    }
   };
+
+  const bindEvents = () => {
+    const $addRootDocumentButton = document.querySelector(".addRootDocumentButton") as HTMLButtonElement;
+    const $documentList = document.querySelector(".documentList") as HTMLUListElement;
+
+    $addRootDocumentButton.addEventListener("click", handleClickAddRootDocumentButton);
+    $documentList.addEventListener("click", handleClickDocumentItem);
+  };
+
+  const documentList = documents
+    .map((document) => {
+      const documentItemComponent = createComponent(DocumentItem, { document });
+
+      return documentItemComponent.element;
+    })
+    .join("");
 
   return {
     element: `
       <div class=${s_sidebar}>
-        <button data-parent-id="null" class="add-document-button ${s_button}" aria-label="새 문서 추가" type="button">+</button>
-        <ul>
-          ${documents
-            .map((document) => {
-              const documentItemComponent = createComponent(DocumentItem, {
-                document,
-                parentId: null,
-              });
-
-              return documentItemComponent.element;
-            })
-            .join("")}
+        <button data-parent-id="null" class="addRootDocumentButton ${s_button}" aria-label="새 문서 추가" type="button">+</button>
+        <ul class="documentList">
+          ${documentList}
         </ul>
       </div>
     `,
