a90ac4663125803482ab8e5a0188137aebb53fe1
Kim-Jaemin420 <jany240@naver.com>
2023-10-23 22:30:00 +0900

    Modify: useEffect 함수 export default

--- src/core/hooks/useEffect.ts
+++ src/core/hooks/useEffect.ts
@@ -9,7 +9,7 @@ const previousDepsMap = new Map<string, DepsArray | []>();
 const hasDepsChanged = (newDeps: DepsArray, oldDeps: DepsArray): boolean =>
   newDeps.some((dep, i) => (isObject(dep) ? !deepEqual(dep, oldDeps[i]) : dep !== oldDeps[i]));
 
-export const useEffect = (callback: EffectCallback, deps: DepsArray) => {
+const useEffect = (callback: EffectCallback, deps: DepsArray) => {
   if (!currentComponent) {
     throw new Error("useEffect는 컴포넌트 안에서만 호출될 수 있습니다..");
   }
@@ -28,3 +28,5 @@ export const useEffect = (callback: EffectCallback, deps: DepsArray) => {
 
   previousDepsMap.set(id, deps);
 };
+
+export default useEffect;
