da83d3113084eea542675b8b2b662c5724e34c4b
Kim-Jaemin420 <jany240@naver.com>
2023-10-23 23:32:52 +0900

    Feat: request body 타입을 제네릭으로 받아 설정

--- src/apis/core.ts
+++ src/apis/core.ts
@@ -1,28 +1,41 @@
 const BASE_URL = process.env.BASE_URL;
 
+interface ApiRequest<T> {
+  url: string;
+  body?: T;
+  options?: RequestInit;
+}
+
 const api = async (endPoint: string, options: RequestInit = {}) => {
   const headers = {
     "x-username": "jaemin",
     "Content-Type": "application/json",
   };
 
-  const response = await fetch(endPoint, {
-    headers,
-    ...options,
-  });
+  try {
+    const response = await fetch(endPoint, {
+      headers,
+      ...options,
+    });
 
-  if (!response.ok) {
-    throw new Error("Network error");
+    if (!response.ok) {
+      throw new Error("Network error");
+    }
+
+    const data = await response.json();
+
+    return await data;
+  } catch (error) {
+    console.error(error);
   }
-
-  const data = await response.json();
-
-  return data;
 };
 
 export default {
-  get: (endPoint: string, options?: RequestInit) => api(`${BASE_URL}${endPoint}`, { ...options, method: "GET" }),
-  post: (endPoint: string, options?: RequestInit) => api(`${BASE_URL}${endPoint}`, { ...options, method: "POST" }),
-  put: (endPoint: string, options?: RequestInit) => api(`${BASE_URL}${endPoint}`, { ...options, method: "PUT" }),
-  delete: (endPoint: string, options?: RequestInit) => api(`${BASE_URL}${endPoint}`, { ...options, method: "DELETE" }),
+  get: <T = unknown>({ url, options }: ApiRequest<T>) => api(`${BASE_URL}${url}`, { ...options, method: "GET" }),
+  post: <T>({ url, body, options }: ApiRequest<T>) =>
+    api(`${BASE_URL}${url}`, { ...options, method: "POST", body: JSON.stringify(body) }),
+  put: <T>({ url, body, options }: ApiRequest<T>) =>
+    api(`${BASE_URL}${url}`, { ...options, method: "PUT", body: JSON.stringify(body) }),
+  delete: <T>({ url, body, options }: ApiRequest<T>) =>
+    api(`${BASE_URL}${url}`, { ...options, method: "DELETE", body: JSON.stringify(body) }),
 };
