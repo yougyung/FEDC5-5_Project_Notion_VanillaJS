90d7f7a2c546f82b1354ff5dd8e3326297d51f8e
Kim-Jaemin420 <jany240@naver.com>
2023-10-23 16:33:06 +0900

    Feat: 두 개의 파라미터가 서로 같은 값인지 깊은 비교하는 함수 구현

--- src/utils/object.ts
+++ src/utils/object.ts
@@ -1,3 +1,24 @@
 export const isObject = (value: unknown): value is Record<string, unknown> => {
   return typeof value === "object" && value !== null;
 };
+
+export const deepEqual = (first: unknown, second: unknown): boolean => {
+  if (!isObject(first) || !isObject(second)) {
+    return first === second;
+  }
+
+  const firstKeys = Object.keys(first);
+  const secondKeys = Object.keys(second);
+
+  if (firstKeys.length !== secondKeys.length) {
+    return false;
+  }
+
+  for (const key of firstKeys) {
+    if (!secondKeys.includes(key) || !deepEqual(first[key], second[key])) {
+      return false;
+    }
+  }
+
+  return true;
+};
