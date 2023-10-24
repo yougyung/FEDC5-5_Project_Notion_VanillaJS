c775ade1de20c5996cd8e09f8f7cae706e73489b
이종혁 <whdgur57117@gmail.com>
2023-10-24 21:03:00 +0900

    [Feat] eslint 규칙 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,27 +2,29 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
   extends: ["airbnb-base", "prettier"],
   rules: {
     "import/prefer-default-export": "off",
     "import/extensions": ["off"],
     "class-methods-use-this": "warn",
+    "no-param-reassign": ["error", { props: false }],
+    "no-underscore-dangle": "allow"
   },
   overrides: [
     {
       env: {
-        node: true,
+        node: true
       },
       files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: "script",
-      },
-    },
+        sourceType: "script"
+      }
+    }
   ],
   parserOptions: {
     ecmaVersion: "latest",
-    sourceType: "module",
-  },
+    sourceType: "module"
+  }
 }
