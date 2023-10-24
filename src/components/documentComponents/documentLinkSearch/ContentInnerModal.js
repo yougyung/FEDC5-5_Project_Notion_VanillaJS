2133296ebe96428836ec69543675ea3bc99db3c8
mibu119 <horang296@gmail.com>
2023-10-24 17:27:51 +0900

    💄 문단 맨 앞에 생기는 모달이면 left = 0px로 변경

--- src/components/documentComponents/documentLinkSearch/ContentInnerModal.js
+++ src/components/documentComponents/documentLinkSearch/ContentInnerModal.js
@@ -5,7 +5,7 @@ export default function ContentInnerModal({ $target, selectionStart, option }) {
   const $contentInnerModal = document.createElement("div");
   $contentInnerModal.className = "content-inner-modal";
   $contentInnerModal.contentEditable = false;
-  $contentInnerModal.style.left = `${selectionStart - 30}px`;
+  $contentInnerModal.style.left = `${selectionStart === 1 ? 0 : selectionStart - 30}px`;
 
   $target.appendChild($contentInnerModal);
 
