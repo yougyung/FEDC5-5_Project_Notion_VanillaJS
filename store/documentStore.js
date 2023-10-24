349f2744dd73b6813f652c3f6df9bd6b1bc95a00
이종혁 <whdgur57117@gmail.com>
2023-10-25 02:48:35 +0900

    [Feat] state에 newDocument deletedDocument 프로퍼티 추가

--- store/documentStore.js
+++ store/documentStore.js
@@ -4,7 +4,7 @@ import {
   createNewDocument,
   deleteDocumentById
 } from "../apis/documents.js"
-const initialState = { documents: [] }
+const initialState = { documents: [], newDocument: null, deletedDocument: null }
 
 const reducer = async (state, action) => {
   switch (action.type) {
@@ -13,9 +13,9 @@ const reducer = async (state, action) => {
       return { ...state, documents }
 
     case "ADD":
-      await createNewDocument(action.payload)
+      const newDocument = await createNewDocument(action.payload)
       const updatedDocuments = await fetchAllDocuments()
-      return { ...state, documents: updatedDocuments }
+      return { ...state, documents: updatedDocuments, newDocument }
 
     case "DELETE":
       await deleteDocumentById(action.payload)
