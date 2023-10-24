ac825eb9e200066e5e62f061bbbbc9482483e7c6
km6293 <alstn7616@naver.com>
2023-10-24 23:42:29 +0900

    feat: 작업 구조 변경

--- src/App.js
+++ src/App.js
@@ -1,7 +1,104 @@
-import PostsPage from "./PostsPage.js"
+import {request} from "./api.js";
+import PostList from "./PostList.js"
 
 export default function App({ $target }){
 
-  const postsPage = new PostsPage({$target})
-  postsPage.setState()
+  const $postListContainer = document.createElement('div');
+  const $editListContainer = document.createElement('div');
+
+  $target.appendChild($postListContainer);
+  $target.appendChild($editListContainer);
+
+  this.state = {
+    postList: [],
+    selectedPostId : null,
+    post: '',
+    postLoading: false
+  }
+
+  const postList = new PostList({
+    $target : $postListContainer,
+    onToggle: async (id, path) => {
+      const allPath = path.split('-');
+      // toggle 누른 post
+      const nowPost = returnPathList([...allPath], this.state.postList)
+
+      // toggle 누른 post의 toggle 값 변경
+      nowPost.isToggled = typeof nowPost.isToggled === 'undefined' ? true : !nowPost.isToggled;
+
+      // 기준 토글의 toggle 값이 true인 경우, 하위 visible을 1단계만 true
+      // 기준 토글의 toogle 값이 false 인 경우, 하위 visible을 전부 false
+      childToggle(nowPost.documents, nowPost.isToggled);
+
+      // 상위들을 불러와서 nowPost랑 합체
+      const updateState = returnMap(this.state.postList, [...allPath], nowPost);
+      const nextState = this.state.postList.map(post => post.id === updateState.id ? updateState : post)
+
+      this.setState({
+        ...this.state,
+        postList: nextState
+      })
+    },
+    onDelete: async(id) => {
+      
+    }
+  })
+
+  // 변경된 것 포함해서 최상위 값 return
+  const returnMap = (postList, path, child) => {
+    if(path.length === 1) return child;
+    const id = path.pop();
+    const parent = returnPathList([...path], postList)
+    const findIndex = parent.documents.findIndex(post => parseInt(post.id) === parseInt(id));
+    parent.documents[findIndex] = child
+    return returnMap(postList, path, parent)
+  }
+
+  // 마지막 path의 post return
+  const returnPathList = (allPath, postList) => {
+    let nextPath = allPath.shift();
+    let nextPost = postList.find(post => post.id == nextPath);
+    if (allPath.length === 0) return nextPost;
+    return nextPost ? returnPathList(allPath, nextPost.documents) : null;
+  }
+
+  // 해당 toggle 값에 따른 자식들 visible 변경
+  function childToggle(documents, state) {
+    if (!Array.isArray(documents)) return;
+    for (const document of documents) {
+      document.isVisible = state;
+      document.isToggled = false;
+      if (!state && Array.isArray(document.documents) && document.documents.length > 0) {
+        childToggle(document.documents, state); 
+      }
+    }
+  }
+
+  const fetchPostList = async () => {
+    const postList = await request('/documents');
+    this.setState({
+      ...this.state,
+      postList
+    })
+  }
+
+  this.setState = nextState => {
+    this.state = nextState
+
+    postList.setState({
+      postList:this.state.postList
+    })
+    
+    this.render()
+  }
+
+  this.render = () => {
+  }
+
+  const init = async () => {
+    await fetchPostList()
+    
+  }
+
+  init()
 }
--- src/PostList.js
+++ src/PostList.js
@@ -1,64 +1,65 @@
-import {request} from "./api.js";
-
-export default function PostList({ $target, initialState }){
-
+export default function PostList({ $target, onToggle, onDelete }){
   const $postList = document.createElement('div');
+  $postList.classList.add('post-list');
   $target.appendChild($postList);
-  this.state = initialState;
 
+  this.state = {
+    postList: [],
+    selectedPost: {
+      id:'',
+      top:'0px',
+      left:'0px',
+    },
+    modalState: false
+  }
+  
   this.setState = (nextState) => {
-    this.state = nextState;
+    this.state = {
+      ...this.state, 
+      ...nextState 
+    }
     this.render();
   }
-
+  
   this.render = () => {
-    $postList.innerHTML = `
-      ${this.state.map(post => renderTree(post)).join('')}
-      <div class="post-add"> + </div>
-      <div class="modal-content">
-        <div >삭제</div>
+    checkArray(this.state.postList) ? 
+      $postList.innerHTML = `
+        ${this.state.postList.map(post => renderTree(post, '')).join('')}
+        <div class="post-add"> + </div>
+        <div class="modal-content" style="display:${this.state.modalState ? 'block' : 'none'};">
+          <div class="post-delete">삭제</div>
+        </div>
+      `
+      : '';
+  }
+
+  const renderTree = (post, path = '') => {
+    path = `${path}${path ? '-' : ''}${post.id}`;
+    const isToggled = post.isToggled || false;
+    const depth = path.match(/-/g)?.length || 0;
+    const isVisible = depth === 0 ? true : post.isVisible || false;
+    const tabLevel = '&nbsp;'.repeat(depth * 2);
+    
+    const nodeHTML = renderTreeNode(post, path, isToggled, isVisible, tabLevel);
+    const childHTML = post.documents.map(child => renderTree(child, path)).join('');
+    return `${nodeHTML}${childHTML}`;
+  }
+
+  const renderTreeNode = (post, path, isToggled, isVisible, tabLevel) => {
+    return `
+      <div data-id="${post.id}" data-path="${path}" style="display: ${isVisible ? 'block' : 'none'}">
+        <span class="post-toggle-button">${tabLevel}${isToggled ? '&#5167;' : '&#5171;'}</span>
+        <span class="post-title">${post.title}</span>
+        <span class="post-load-more-button"> ··· </span>
+        <span class="post-add-child"> + </span>
+        ${isVisible && isToggled && post.documents.length === 0 ? `<div>${tabLevel}하위 페이지 없음</div>` : ''}
       </div>
     `;
   }
-
-  const checkChild = (documents) => Array.isArray(documents) && documents.length > 0;
-
-  function renderTree(post, path = ''){
-    const isToggled = post.isToggled || false;
-    
-    path = path === '' ? post.id.toString() : `${path}-${post.id}`; // path를 수정
-    const depth = path.match(/-/g)?.length || 0;
-    const tabLevel = '&nbsp;'.repeat(depth * 2);
-
-    const childrenHTML = post.documents.map(child => renderTree(child, path)).join('');
-
-    return `
-      <div data-id="${post.id}" data-path="${path}">
-        <span class="post-toggle-button">${tabLevel}${isToggled ? '&#5167;' : '&#5171;'}</span>
-        <span class="post-title">${post.title}</span>
-        <span class="post-load-more-button" > ··· </span>
-        <span class="post-add-child"> + </span>
-      </div>
       
-      ${isToggled ? childrenHTML : ''}
-      ${isToggled && post.documents.length === 0 ? `${tabLevel}하위 페이지 없음` : ''}
-    `
-  }
+  const checkArray = (documents) => Array.isArray(documents) && documents.length > 0;
 
-
-  function togglePostState(posts, postId){
-    return posts.map(post => {
-      if(post.id === postId){
-        return { ...post, isToggled: !post.isToggled };
-      }else if(checkChild(post.documents)){
-        return { ...post, documents: togglePostState(post.documents, postId) };
-      }
-      return post;
-    });
-  }
-
-
-  $postList.addEventListener('click', async (e) => {
+  $target.addEventListener('click', async (e) => {
     const $div = e.target.closest('div')
 
     if($div){
@@ -66,36 +67,39 @@ export default function PostList({ $target, initialState }){
       const {id, path} = $div.dataset;
 
       if(className === 'post-toggle-button'){
-        const nextState = togglePostState(this.state, parseInt(id));
-        this.setState(nextState);
+        // 포스트 토글 클릭 시의 동작
+        onToggle(id, path, findChild(path))
+
       }else if(className === 'post-title'){
         // 포스트 제목 클릭 시의 동작
-      }else if(className === 'post-load-more-button'){
-        // 더보기 버튼 클릭 시의 동작
-        const $modal = $postList.querySelector('.modal-content');
-        const $button = e.target;
 
-        $modal.style.top = $button.offsetTop + $button.offsetHeight + 'px';
-        $modal.style.left = $button.offsetLeft + 'px';
-        $modal.style.display = $modal.style.display === 'none' ? 'block' : 'none';
+      }else if(className === 'post-load-more-button'){
+        // 포스트 더보기 클릭 시의 동작
+        const modalResult = $postList.querySelector(".modal-content").style.display === 'none';
+        const selectedPost = modalResult 
+          ? { id, top: e.target.offsetTop + e.target.offsetHeight+'px', left: e.target.offsetLeft+'px'} 
+          : { id: '', top: '0px', left: '0px' };
+
+        this.setState({
+          ...this.state,
+          selectedPost,
+          modalState: modalResult
+        });
 
       }else if(className === 'post-add-child') {
         // 자식 추가 버튼 클릭 시의 동작
-        const createdPost = await request(`/documents`, {
-          method: 'POST',
-          body: JSON.stringify({
-            'title': '제목 없음',
-            'parent': id
-          })
-        })
-
-        const nextState = await togglePostState(this.state, parseInt(id), createdPost)
-        this.setState(nextState)
+        
+      }else if(className === 'post-delete'){
+        // 삭제 버튼
       }
     }
 
   })
 
-  this.render();
+  const findChild = (path) => {
+    return Array.from($postList.querySelectorAll('div[data-path*="'+path+'"]'))
+                .map(div => div.getAttribute('data-id'));
+  }
 
+  this.render();
 }
