e990ad3ff15022b19a9126397388d4d7583f4796
Kim-Jaemin420 <jany240@naver.com>
2023-10-25 19:42:33 +0900

    Feat: export childrenDocumentLinks

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,5 @@ export { default as DocumentItem } from "./DocumentItem/DocumentItem";
 export { default as Sidebar } from "./Sidebar/Sidebar";
 export { default as Editor } from "./Editor/Editor";
 export { default as UserGuide } from "./UserGuide/UserGuide";
+export { default as ChildDocumentLinks } from "./ChildDocumentLinks/ChildDocumentLinks";
 export { default as NotFound } from "./NotFound/NotFound";
