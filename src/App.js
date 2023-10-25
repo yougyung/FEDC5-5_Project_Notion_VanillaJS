2b891837d86128275e4a621d53b802c6b514eee3
nnahyunn <nahyun0104@cau.ac.kr>
2023-10-25 22:36:42 +0900

    feat: App 구성

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import Sidebar from "./components/Sidebar/Sidebar.js";
+import PostEditPage from "./components/Editor/PostEditPage.js";
+import { initRouter } from "./utils/router.js";
+
+export default function App({ $target }) {
+  const $sidebar = document.createElement("div");
+  const $postEditPage = document.createElement("div");
+
+  $target.appendChild($sidebar);
+  $target.appendChild($postEditPage);
+
+  const sidebar = new Sidebar({ $target: $sidebar });
+
+  const postEditPage = new PostEditPage({
+    $target: $postEditPage,
+    initialState: {
+      postId: "new",
+      post: {
+        title: "",
+        content: "",
+      },
+    },
+  });
+
+  this.route = () => {
+    const { pathname } = window.location;
+    if (pathname.indexOf("/documents/") === 0) {
+      const [, , postId] = pathname.split("/");
+      postEditPage.setState({ postId });
+    } else {
+      sidebar.setState();
+    }
+  };
+
+  this.route();
+
+  initRouter(() => this.route());
+}
