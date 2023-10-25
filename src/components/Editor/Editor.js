f9d00105a3e1e9f9d53a6850d02a845c3843aa23
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2023-10-25 11:05:57 +0900

    feat: editor title, content 수정 시 setState 기본 동작 추가

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -2,7 +2,7 @@ export default function Editor({ $target, initialState }) {
   const $editor = document.createElement("div");
   $editor.className = "editor";
   $editor.innerHTML = `
-    <input class='editor_name' type="text" name="title" placeholder='제목을 입력하세요'/>
+    <input class='editor_title' type="text" name="title" placeholder='제목을 입력하세요'/>
     <div class='editor_content' name="content" contentEditable='true'></div>
   `;
   this.state = initialState;
@@ -12,5 +12,18 @@ export default function Editor({ $target, initialState }) {
     this.state = nextState;
     this.render();
   };
-  this.render = () => {};
+  this.render = () => {
+    console.log(this.state);
+    $editor.querySelector("[name=title]").value = this.state.title;
+
+    const $content = $editor.querySelector("[name=content]");
+  };
+
+  $editor.querySelector("[name=title]").addEventListener("keyup", (e) => {
+    this.setState({ ...this.state, title: e.target.value });
+  });
+
+  $editor.querySelector("[name=content]").addEventListener("input", (e) => {
+    this.setState({ ...this.state, content: e.target.innerHTML });
+  });
 }
