13e6a954d76c59199d19dcf1ef68361d29dd93fd
dy6578ekdbs <dy6578ekdbs@gmail.com>
2023-10-25 11:26:16 +0900

    ✨ feat  : storage hook

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,9 +1,6 @@
 const storage = window.localStorage;
 
 export const setItem = (key, value) => {
-  /**
-   * 로컬 스토리지 용량 제한을 넘으면 에러가 발생할 수 있으므로 try-catch 추가
-   */
   try {
     storage.setItem(key, JSON.stringify(value));
   } catch (e) {
