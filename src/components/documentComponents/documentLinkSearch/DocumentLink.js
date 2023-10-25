f41cfd021dea3bcb169562db5f3ec76485bc6b73
mibu119 <horang296@gmail.com>
2023-10-25 13:05:04 +0900

    ✨ 🌱 문서에서 만들어지는 링크 documentlink 생성

---
+++ src/components/documentComponents/documentLinkSearch/DocumentLink.js
@@ -0,0 +1,20 @@
+import { push } from "../../../utils.js";
+
+export default function DocumentLink({ $target, id, title }) {
+  const $documentLinkWrapper = document.createElement("div");
+  $documentLinkWrapper.contentEditable = false;
+  $target.appendChild($documentLinkWrapper);
+
+  const $documentLink = document.createElement("a");
+  $documentLink.className = "document-link";
+  $documentLink.innerText = title;
+
+  $documentLinkWrapper.appendChild($documentLink);
+
+  $documentLink.addEventListener("click", (event) => {
+    event.preventDefault();
+    push(`${id}`);
+  });
+
+  return $documentLink;
+}
