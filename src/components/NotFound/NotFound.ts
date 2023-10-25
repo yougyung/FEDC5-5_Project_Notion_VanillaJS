e4651c19ac8ac3dc8fd4a46d5d0bbb5c1740ffd9
Kim-Jaemin420 <jany240@naver.com>
2023-10-26 00:29:26 +0900

    Feat: 404 페이지 스타일 추가 및 시멘틱 태그 사용

--- src/components/NotFound/NotFound.ts
+++ src/components/NotFound/NotFound.ts
@@ -1,10 +1,15 @@
+import styles from "./nofFound.module.scss";
+
+const { s_notFoundContainer, s_notFoundMessage, s_notFoundDescription } = styles;
+
 function NotFound() {
   return {
     element: `
-      <div>
-      404 페이지를 찾을 수 없습니다.
-      올바른 url을 입력했나요?
-      </div>
+      <section class="${s_notFoundContainer}">
+        <header class="whiteSpace">404</header>
+        <p class="${s_notFoundMessage}"> 페이지를 찾을 수 없습니다.</p>
+        <p class="${s_notFoundDescription}">올바른 URL을 입력했나요?</p>
+      </section>
     `,
   };
 }
