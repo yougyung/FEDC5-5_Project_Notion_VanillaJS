df7962a23370269950973b4f0cbaa53c46aeaf74
dy6578ekdbs <dy6578ekdbs@gmail.com>
2023-10-25 11:22:01 +0900

    ✨ feat  : Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,23 @@
-import style from "./header.module.css";
+import "./header.css";
 import { navigate } from "../../utils/router";
-import { $ } from "../../utils/DOM";
+import { $ } from "../../utils/DOM/selector.js";
 
 /** 지금 선택된 document의 id */
 export default function Header({ $target, initialState }) {
   const $header = document.createElement("div");
+  $header.className = "header";
+  $header.innerHTML = `
+  <div class="header-title-container"></div>
+
+  <div class="header-btn-container">
+  <span>공유</span>
+  <span>댓글</span>
+  <span>시간</span>
+  <span>별</span>
+  <span>...</span>
+  </div>`;
+
+  $target.appendChild($header);
 
   this.state = initialState;
 
@@ -13,19 +26,16 @@ export default function Header({ $target, initialState }) {
     this.render();
   };
 
+  /** 상위 document의 title과 id를 찾는 함수 */
   const findParent = (headerTitleArray, $now) => {
-    const nowTitle = $now
-      .querySelector(".document")
-      .querySelector("span").textContent;
-
+    const nowTitle = $(".document", $now).textContent;
     headerTitleArray.push({ id: this.state, title: nowTitle });
-    let $parent = $now.parentElement;
 
+    let $parent = $now.parentElement; // 상위 dom으로 올라가기
+
+    // root document까지 올라가기
     while ($parent && $parent.classList.contains("document-title")) {
-      const parentTitle = $parent
-        .querySelector(".document")
-        .querySelector("span").textContent;
-
+      const parentTitle = $(".document", $parent).textContent;
       const { id } = $parent.dataset;
       headerTitleArray.push({ id: id, title: parentTitle });
       $parent = $parent.parentElement;
@@ -38,29 +48,29 @@ export default function Header({ $target, initialState }) {
     const headerTitleArray = [];
     const $now = $(`ul[data-id="${this.state}"]`);
 
-    const parentTitles = findParent(headerTitleArray, $now);
-    console.log("결과", parentTitles);
+    if (!$now) {
+      $(".header-title-container").innerHTML = "";
+      return;
+    }
 
-    $header.innerHTML = `${parentTitles
+    const parentTitles = findParent(headerTitleArray, $now);
+
+    const headerElements = `${parentTitles
       .map(
         parent =>
           `<p data-id="${parent.id}" class="header-p" >${parent.title}</p>`,
       )
-      .join("")}`;
+      .join("<span>/</span>")}`;
 
-    $target.appendChild($header);
+    $(".header-title-container").innerHTML = headerElements;
   };
 
   $header.addEventListener("click", ({ target }) => {
     const $p = target.closest(".header-p");
-    console.log("클릭");
 
     if ($p) {
       const { id } = $p.dataset;
-
       navigate(`/documents/${id}`);
     }
   });
-
-  // this.render();
 }
