d4e8468cdeee54d61fb01087b2009eb245586cff
suyoung <joyswim.dev@gmail.com>
2023-10-25 18:50:52 +0900

    feat: EditPage Editor 컴포넌트 생성

--- src/components/EditPage.js
+++ src/components/EditPage.js
@@ -1,14 +1,22 @@
-export default function DocumentEditor({ $target, initialState }) {
+import Editor from './Editor.js'
+
+const EDITOR_DUMMY = {
+  title: '처음 작성하는 제목',
+  content: '처음 작성하는 내용'
+}
+
+export default function EditPage({ $target, initialState }) {
   const $page = document.createElement('div')
   $page.id = "document-contents"
-  
+
+  new Editor({
+    $target: $page,
+    initialState: EDITOR_DUMMY
+  })
+
   this.render = () => {
-    $page.innerHTML = `
-    <div>여기는 EditPage</div>
-    <input type="text" name="title" style="width: 600px;" placeholder="제목 없음" value="타이틀" />
-        <textarea name="content" style="width: 600px; height: 400px;" placeholder="내용을 입력하세요">내용을 입력하자</textarea>
-    `
     $target.appendChild($page)
   }
+
   this.render()
 }
