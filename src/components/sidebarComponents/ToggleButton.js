c566f65d2484eb3aebee13542761cf71a6db2570
mibu119 <horang296@gmail.com>
2023-10-25 17:59:48 +0900

    🔥 togglebutton에서 사용안하는 코드 제거

--- src/components/sidebarComponents/ToggleButton.js
+++ src/components/sidebarComponents/ToggleButton.js
@@ -2,33 +2,13 @@ export const ToggleButton = () => {
   let isOpen = false;
   const openIds = [];
 
-  //   const findDocumentById = (id, documents) => {
-  //     for (const document of documents) {
-  //       if (document.id === id) {
-  //         return document;
-  //       }
-
-  //       if (document.documents.length > 0) {
-  //         const foundDocument = findDocumentById(id, document.documents);
-  //         if (foundDocument) {
-  //           return foundDocument;
-  //         }
-  //       }
-  //     }
-
-  //     return null;
-  //   };
-
   const toggleDocument = (id) => {
     const index = openIds.indexOf(id);
-    console.log(openIds, index);
     if (index === -1) {
       openIds.push(id);
     } else if (index === 0) {
       openIds.splice(index, 1);
     }
-
-    // renderList(documents, 0);
   };
 
   const openDocumentList = (id) => {
