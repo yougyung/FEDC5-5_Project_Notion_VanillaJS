fafe0ad6af3b9cc132a99dca1b6380f89adc3d96
dy6578ekdbs <dy6578ekdbs@gmail.com>
2023-10-25 11:22:07 +0900

    ✨ feat  : OnBoarding component

---
+++ src/components/onBoarding/OnBoarding.js
@@ -0,0 +1,28 @@
+import "./onboarding.css";
+import icon from "../../img/profile.png";
+
+export default function OnBoarding({ $target }) {
+  const $onBoarding = document.createElement("div");
+  $onBoarding.className = "onboarding";
+  $onBoarding.innerHTML = `
+  <img src="${icon}"/>
+<h1>Maru's Notion</h1>
+<p>This is a Notion clone by Dayun.</p>
+<p>Feel free to create documents! 📖</p>
+`;
+
+  this.display = () => {
+    const { pathname } = window.location;
+    if (pathname === "/") {
+      $onBoarding.style.display = "block";
+    } else {
+      $onBoarding.style.display = "none";
+    }
+  };
+
+  this.render = () => {
+    $target.appendChild($onBoarding);
+  };
+
+  this.render();
+}
