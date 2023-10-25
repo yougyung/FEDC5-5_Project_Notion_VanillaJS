e94a4415353cdbc7198f7b22f8ea831fb2fd7955
suyoung <joyswim.dev@gmail.com>
2023-10-25 18:50:13 +0900

    feat: DocsPage DocsList 컴포넌트 생성

--- src/components/DocsPage.js
+++ src/components/DocsPage.js
@@ -1,21 +1,47 @@
-export default function documentsPage({
-  $target
-}) {
+import DocsList from './DocsList.js'
+
+const DUMMY_LIST = [
+  {
+    "id": 1, // Document id
+    "title": "노션을 만들자", // Document title
+    "documents": [
+      {
+        "id": 2,
+        "title": "1 하위페이지",
+        "documents": [
+          {
+            "id": 3,
+            "title": "1-2 하위페이지",
+            "documents": []
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": 4,
+    "title": "hello!",
+    "documents": []
+  }
+]
+
+export default function DocsPage({ $target }) {
   const $page = document.createElement('div')
   $page.id = 'sidebar'
   
+  new DocsList({
+    $target: $page,
+    initialState: DUMMY_LIST,
+    onClickAddRoot: () => {
+      console.log('페이지추가 눌림')
+    },
+    onClickAddSub: () => {
+      console.log('하위페이지추가 눌림')
+    },
+  })
+
   this.render = () => {
-    $page.innerHTML = `
-    <div>여기는 DocsPage</div>
-    <ul>
-      <li>루트 1</li>
-      <li>루트 2</li>
-      <ul>
-        <li>하위 페이지</li>
-      </ul>
-    </ul>
-    <div>페이지 추가 +</div>
-    `
+    console.log($page)
     $target.appendChild($page)
   }
   this.render()
