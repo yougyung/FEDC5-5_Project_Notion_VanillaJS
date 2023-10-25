d0120fbd331ddaf99508295c761d9d5939c92289
JangGwon <tlsakch510@naver.com>
2023-10-25 21:15:10 +0900

    feat : MenuItem Element 삭제 기능 구현

--- src/component/sidebar/MenuItem.js
+++ src/component/sidebar/MenuItem.js
@@ -12,19 +12,23 @@ export default class MenuItem {
         this.parentListElement = document.createElement('li');
         this.slotButtonElement = document.createElement('button');
         this.documentNameLabelElement = document.createElement('label');
-        const addButtonElement = document.createElement('button');
         this.childListElement = document.createElement('ul');
+        const addButtonElement = document.createElement('button');
+        const deleteButtonElement = document.createElement('button');
+
 
         parentElement.appendChild(this.parentListElement);
         this.parentListElement.appendChild(this.slotButtonElement);
         this.parentListElement.appendChild(this.documentNameLabelElement);
         this.parentListElement.appendChild(addButtonElement);
+        this.parentListElement.appendChild(deleteButtonElement);
         this.parentListElement.appendChild(this.childListElement);
 
         this.parentListElement.id = this.item.id;
         this.documentNameLabelElement.textContent = this.item.title;
-        this.documentNameLabelElement.id = `documentbtn${item.id}`
-        this.slotButtonElement.id = `slotbtn${item.id}`
+        this.documentNameLabelElement.id = `documentbtn${item.id}`;
+        this.slotButtonElement.id = `slotbtn${item.id}`;
+        deleteButtonElement.id = `deletebtn${item.id}`;
         this.slotButtonElement.textContent = this.isSlotOpen ? "^" : ">";
         this.childListElement.style.display = "none";
         addButtonElement.id = `addbtn${item.id}`
@@ -52,6 +56,20 @@ export default class MenuItem {
                     this.arr.push(new MenuItem({ id, title, documents: [] }, this.childListElement));
                 })
             }
+        });
+        deleteButtonElement.addEventListener('click', (event) => {
+            if (event.target.id === "deletebtn" + this.item.id) {
+                const dfs = (node) => {
+                    node.documents.map((documentItem) => {
+                        dfs(documentItem);
+                    });
+                    request(`/documents/${node.id}`, {
+                        method: `DELETE`
+                    });
+                };
+                dfs(this.item);
+            }
+            parentElement.removeChild(this.parentListElement);
         });
     }
 
