adc20501a33c5a1440044a2466910a9ca0cde21e
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2023-10-25 09:26:12 +0900

    feat: 기본 Editor backbone 구현

---
+++ src/components/Editor/Editor.js
@@ -0,0 +1,16 @@
+export default function Editor({ $target, initialState }) {
+  const $editor = document.createElement("div");
+  $editor.className = "editor";
+  $editor.innerHTML = `
+    <input class='editor_name' type="text" name="title" placeholder='제목을 입력하세요'/>
+    <div class='editor_content' name="content" contentEditable='true' placeholder='내용을 입력하세요.'></div>
+  `;
+  this.state = initialState;
+  $target.appendChild($editor);
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+  this.render = () => {};
+}
