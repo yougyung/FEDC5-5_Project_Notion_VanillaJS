49f37e863b55aa01e9cf716579dc91e58e671d0f
nnahyunn <nahyun0104@cau.ac.kr>
2023-10-25 23:12:48 +0900

    fix: 에러 메세지 수정

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,16 +6,16 @@ export const request = async (url, options = {}) => {
       ...options,
       headers: {
         "Content-Type": "application/json",
-        "x-username": "kimnahyun",
+        "x-username": "nahyun",
       },
     });
 
     if (res.ok) {
       return await res.json();
     }
-
-    throw new Error("API 처리 중 뭔가 이상합니다");
+    throw new Error("API 처리 중 뭔가 이상합니다!");
   } catch (e) {
-    alert(e.message);
+    console.error(e.message);
+    alert("서버와의 통신 중 문제가 발생했습니다.");
   }
 };
