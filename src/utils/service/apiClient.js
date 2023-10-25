1410ec5e6bfd8a584401922211e3b45d14b9d083
dy6578ekdbs <dy6578ekdbs@gmail.com>
2023-10-25 11:20:01 +0900

    ✨ feat : apiClient - fetch hook

---
+++ src/utils/service/apiClient.js
@@ -0,0 +1,22 @@
+const API_END_POINT = "https://kdt-frontend.programmers.co.kr/documents";
+const X_USERNAME = "maru";
+
+export const apiClient = async (url = "", options = {}) => {
+  try {
+    const res = await fetch(`${API_END_POINT}${url}`, {
+      ...options,
+      headers: {
+        "Content-Type": "application/json",
+        "x-username": X_USERNAME,
+      },
+    });
+    if (res.ok) {
+      const json = await res.json();
+      return json;
+    }
+
+    throw new Error("Api 호출 오류");
+  } catch (err) {
+    alert(err.message);
+  }
+};
