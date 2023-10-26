e32b4c6164397d79d0ec98231cc1848fb2fda9b2
HoberMin <102784200+HoberMin@users.noreply.github.com>
2023-10-26 21:39:08 +0900

    feat : documents 리스트에 이벤트 버블링을 통한 클릭이벤트

--- src/page/ListPage.js
+++ src/page/ListPage.js
@@ -31,6 +31,8 @@ export default function ListPage({ $target, initialState }) {
     this.state = nextState;
     listHeader.setState(this.state.isLoading);
     this.render();
+    listItem.setState(this.state.documentsTree);
+  };
   this.render = () => {
     $docList.innerHTML = ``;
   };
@@ -57,3 +59,74 @@ export default function ListPage({ $target, initialState }) {
     push(`/documents/${req.id}`);
   });
 
+  $docList.addEventListener("mouseover", (e) => {
+    const $li = e.target.closest("li");
+    if ($li) {
+      const $removeButton = $li.querySelector(".remove-folder");
+      const $addButton = $li.querySelector(".add-folder");
+
+      $removeButton.style.display = "inline";
+      $addButton.style.display = "inline";
+    }
+  });
+
+  $docList.addEventListener("mouseout", (e) => {
+    const $li = e.target.closest("li");
+    if ($li) {
+      const $removeButton = $li.querySelector(".remove-folder");
+      const $addButton = $li.querySelector(".add-folder");
+
+      $removeButton.style.display = "none";
+      $addButton.style.display = "none";
+    }
+  });
+
+  // li 내 이벤트 버블링
+  $docList.addEventListener("click", async (e) => {
+    const $li = e.target.closest("li");
+
+    if ($li) {
+      const { id } = $li.dataset;
+      const $div = $li.querySelector(".div-ul-li-container");
+      const $toggleButton = $li.querySelector(".toggle-folder");
+      const $span = $li.querySelector(".non-documents");
+
+      if (e.target.classList.contains("toggle-folder")) {
+        if ($div) {
+          $div.style.display = $div.style.display === "none" ? "block" : "none";
+        } else {
+          $span.style.display =
+            $span.style.display === "none" ? "block" : "none";
+        }
+        $toggleButton.innerHTML = $toggleButton.innerHTML === "˅" ? "˃" : "˅";
+      }
+
+      //기존 파일 삭제 => Documents 리렌더링
+      else if (e.target.classList.contains("remove-folder")) {
+        await request(`/documents/${id}`, {
+          method: "DELETE",
+        });
+        await this.fetchDocumentsList();
+        push(`/`);
+      }
+
+      //새로운 파일 추가 => Documents 리렌더링
+      else if (e.target.classList.contains("add-folder")) {
+        const req = await request("/documents", {
+          method: "POST",
+          body: JSON.stringify({ parent: id, title: "새로 생성된 파일" }),
+        });
+        await this.fetchDocumentsList();
+        push(`/documents/${req.id}`);
+      }
+
+      // 본인을 제외한 다른 파일을 클릭했을 때 해당에디터 열기
+      else if (
+        e.target.classList.contains("click-folder") &&
+        id !== window.location.pathname.split("/").pop()
+      ) {
+        push(`/documents/${id}`);
+      }
+    }
+  });
+}
