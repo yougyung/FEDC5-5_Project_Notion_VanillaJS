c827df0f30739f90273da39e9e3d59ce93f03cb7
JangGwon <tlsakch510@naver.com>
2023-10-26 22:32:30 +0900

    feat :  historyApi를 이용한 spa 페이지 라우팅 구현

--- src/component/main/Page.js
+++ src/component/main/Page.js
@@ -9,11 +9,21 @@ export default class Page {
         this.PageTitle = new PageTitle(pageElement);
         this.editor = new Editor({ pageElement, onChangeTitle });
         rootElement.appendChild(pageElement);
+
+        window.addEventListener('popstate', e => {
+            const { pathname } = location;
+            const path = pathname.replace(window.location.origin, '');
+            const id = path.replace('/documents/', "");
+            this.setDocuments(id);
+        });
     }
 
     setDocument(id) {
         this.id = id;
         this.editor.changeDocument(id);
         this.PageTitle.setTitle(id);
+
+        const path = `/documents/${this.id}`;
+        history.pushState(null, null, path);
     }
 }
