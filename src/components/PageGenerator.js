c4a7c67ccc8d7926e2a95c42cb4baec970da0c42
yeonkyoung <keep.ykk@gmail.com>
2023-10-26 22:39:24 +0900

    chore: 새 페이지 생성 탭 파일 추가

---
+++ src/components/PageGenerator.js
@@ -0,0 +1,14 @@
+import { SVG_CIRCLE_ADD } from '../constants/index.js';
+
+export default function PageGenerator({ $target, initialState, onCreatePage }) {
+	const $div = document.createElement('div');
+	$div.setAttribute('class', 'create_page_container');
+	$div.innerHTML = `<button name='circle_add' class='circle_add'>${SVG_CIRCLE_ADD}</button><span class="create_page text" >새 페이지</span>`;
+
+	$target.appendChild($div);
+	this.state = initialState;
+
+	this.setState = (nextState) => {
+		this.state = nextState;
+	};
+}
