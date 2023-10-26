eadac2f0d2db3fb45e00437ffa97cd17fc8219e8
seongminn <berry9901@gmail.com>
2023-10-26 20:40:20 +0900

    chore: storage 내부 코드 정리

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,31 +1,30 @@
 // eslint-disable-next-line max-lines-per-function
 export const initStorage = (key, defaultValue = [], storage = window.localStorage) => {
   const getItem = () => JSON.parse(storage.getItem(key)) || defaultValue;
+
   const setItem = (value) => storage.setItem(key, JSON.stringify(value));
+
   const appendItem = (value) => {
     const storedValue = getItem();
+    const updatedValue = [...new Set([...storedValue, value])];
 
-    storage.setItem(key, JSON.stringify([...new Set([...storedValue, value])]));
+    storage.setItem(key, JSON.stringify(updatedValue));
   };
+
   const toggleItem = (value) => {
     const storedValue = getItem();
+    const updatedValue = storedValue.includes(value)
+      ? storedValue.filter((item) => item !== value)
+      : [...storedValue, value];
 
-    if (storedValue.length < 1)
-      return storage.setItem(key, JSON.stringify([...storedValue, value]));
-
-    const index = storedValue.indexOf(value);
-
-    if (index < 0) {
-      storage.setItem(key, JSON.stringify([...storedValue, value]));
-    } else {
-      storage.setItem(key, JSON.stringify([...storedValue.filter((_, idx) => index !== idx)]));
-    }
+    storage.setItem(key, JSON.stringify(updatedValue));
   };
 
   const deleteUniqueItem = (value) => {
     const storedValue = getItem();
+    const updatedValue = [...storedValue.filter((id) => id !== value)];
 
-    storage.setItem(key, JSON.stringify([...storedValue.filter((id) => id !== value)]));
+    storage.setItem(key, JSON.stringify(updatedValue));
   };
 
   return { getItem, setItem, appendItem, toggleItem, deleteUniqueItem };
