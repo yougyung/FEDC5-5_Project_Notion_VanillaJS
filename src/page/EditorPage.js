5a8e4c0fe9ae360ed280f64625690c571fe6b77e
HoberMin <102784200+HoberMin@users.noreply.github.com>
2023-10-26 21:43:09 +0900

    feat : editor기능구현

--- src/page/EditorPage.js
+++ src/page/EditorPage.js
@@ -19,21 +19,70 @@ export default function EditorPage({ $target, initialState, onListRender }) {
     $target: $eidtor,
     initialState: { ...this.state, isLoading: false },
   });
+
+  let timer = null;
+
   const editor = new Editor({
     $target: $eidtor,
     initialState: post,
+    onEditing: (post) => {
       setItem(docLocalSaveKey, {
         ...post,
         tempSaveDate: new Date(),
       });
+      if (timer !== null) {
+        clearTimeout(timer);
+      }
+      editHeader.setState({
+        isLoading: true,
+        selectedDocument: this.state.selectedDocument,
+      });
+      timer = setTimeout(async () => {
+        await request(`/documents/${post.id}`, {
+          method: "PUT",
+          body: JSON.stringify(post),
+        });
+        editHeader.setState({
+          isLoading: false,
+          selectedDocument: this.state.selectedDocument,
+        });
         removeItem(docLocalSaveKey);
+        onListRender();
+      }, 2000);
     },
   });
+  this.setState = async (nextState) => {
     if (this.state.selectedID !== nextState.selectedID) {
       docLocalSaveKey = `temp-doc-content-${nextState.selectedID}`;
+      this.state = nextState;
+      await fetchDocument();
+      return;
+    }
+    this.state = nextState;
+    this.render();
+
+    editor.setState(this.state.selectedDocument || { title: "", content: "" });
+    editHeader.setState(
+      this.state || { selectedDocument: [], isLoading: false }
+    );
+  };
+
   this.render = () => {
     $target.appendChild($eidtor);
   };
+
+  const fetchDocument = async () => {
+    //state에 selectedid만 받아오면 되는것 같다.
+    // id를 갖고 data를 가져오는
+    const { selectedID } = this.state;
+
+    if (selectedID !== "main") {
+      const selectedDocument = await request(`/documents/${selectedID}`);
+      const tempPost = getItem(docLocalSaveKey, {
+        title: "",
+        content: "",
+      });
+
       if (
         tempPost.tempSaveDate &&
         tempPost.tempSaveDate > selectedDocument.updatedAt
@@ -47,4 +96,14 @@ export default function EditorPage({ $target, initialState, onListRender }) {
           return;
         }
       }
+
+      this.setState({
+        ...this.state,
+        selectedDocument,
+        isLoading: false,
+      });
+    }
+  };
+
+  fetchDocument();
 }
