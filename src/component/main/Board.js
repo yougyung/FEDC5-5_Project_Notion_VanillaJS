348e870a269287120ecfb11f15e46dc78b775e35
JangGwon <tlsakch510@naver.com>
2023-10-26 11:14:26 +0900

    feat : BoardEditor 개행 문제 수정 및 H3 태그 추가

--- src/component/main/Board.js
+++ src/component/main/Board.js
@@ -4,7 +4,7 @@ export default class Board {
 
     constructor({ rootElement, onChangeTitle }) {
         this.onChangeTitle = onChangeTitle.bind(this);
-        this.editorElement = document.createElement('div');
+        this.editorElement = document.createElement('form');
         this.editorElement.className = "textEditor";
         this.editorElement.setAttribute("contenteditable", "true");
         rootElement.appendChild(this.editorElement);
@@ -18,13 +18,31 @@ export default class Board {
         const req = await request(`/documents/${this.id}`, { method: `GET` });
         this.title = req.title;
         this.content = req.content;
-        this.editorElement.innerHTML = this.content
-        this.editorElement.addEventListener("input", (e) => {
-            const textHtml = e.target.innerHTML;
-            const startIndex = textHtml.indexOf('<div>');
-            const title = startIndex === -1 ? e.target.innerHTML : textHtml.substring(0, startIndex);
-            const textContent = textHtml.replaceAll(/<div>/g, "<br>").replaceAll(/<\/div>/g, "")
-            this.updateDocument(title, textContent);
+        this.editorElement.innerHTML = this.content;
+        this.editorElement.addEventListener("keyup", (e) => {
+            const textHTML = e.target.innerHTML;
+            const findDiv = this.editorElement.querySelector('div');
+            const findH3 = textHTML.split("###&nbsp;");
+
+            if (findDiv) {
+                e.preventDefault();
+                const newLine = document.createElement('br');
+                this.editorElement.removeChild(findDiv);
+                this.editorElement.appendChild(newLine);
+            }
+            if (findH3.length > 1) {
+                e.preventDefault();
+                e.target.innerHTML = findH3[0] + `<h3 id="cur">&nbsp;</h3>`;
+                const selection = window.getSelection();
+                const range = document.createRange();
+                const h3Node = document.getElementById('cur');
+                range.selectNode(h3Node);
+                h3Node.id = "";
+                selection.removeAllRanges();
+                selection.addRange(range);
+            }
+            console.log(e.target.innerHTML);
+            this.updateDocument('제목 없음', textHTML);
         });
     }
     async updateDocument(title, content) {
