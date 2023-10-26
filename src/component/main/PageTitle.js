c23652a9b4dbccd94faeee46d11e5c030926c85f
JangGwon <tlsakch510@naver.com>
2023-10-27 06:30:37 +0900

    fix : 404 페이지갔을때 사이드바 크기 줄어드는 버그 수정

--- src/component/main/PageTitle.js
+++ src/component/main/PageTitle.js
@@ -9,16 +9,11 @@ export default class PageTitle {
 
     setTitle(id) {
         if (!id) return;
-        const titleList = this.findParentTracking(id);
-        titleList.map((titleName) => {
-            const newItem = document.createElement('p');
-            newItem.textContent = titleName;
-        });
-        this.titleElement.textContent
+        const title = this.findParentTracking(id);
         this.titleElement.textContent = title.join(" / ");
     }
 
-    findParentTracking(id) {
+    findParentTracking(id) { 
         let node = document.querySelector(`#l${id}`);
         const documentNodeTrackArr = [node.getAttribute("titlename")]
         while (node.parentElement.parentElement.getAttribute("titlename")) {
