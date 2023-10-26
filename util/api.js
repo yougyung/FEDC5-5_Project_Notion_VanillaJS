d94f9d170201eb40e4a9dcb3698a1f90b8013cb4
HoberMin <102784200+HoberMin@users.noreply.github.com>
2023-10-26 21:31:50 +0900

    feat : fetch요청 request

---
+++ util/api.js
@@ -0,0 +1,17 @@
+export const API_END_POINT = "https://kdt-frontend.programmers.co.kr";
+
+export const request = async (url, options = {}) => {
+  try {
+    const res = await fetch(`${API_END_POINT}${url}`, {
+      ...options,
+      headers: { "Content-Type": "application/json", "x-username": "sonhomin" },
+    });
+    if (res.ok) {
+      const json = await res.json();
+      return json;
+    }
+    throw new Error("API 호출 오류");
+  } catch (e) {
+    alert(e.message);
+  }
+};
