1a3c9541dd948ba54ecab6ef138f7109860164e5
HoberMin <102784200+HoberMin@users.noreply.github.com>
2023-10-26 21:37:58 +0900

    feat : 로딩상태에 따른 사이드바 헤더

--- src/component/ListHeader.js
+++ src/component/ListHeader.js
@@ -1,1 +1,24 @@
 export default function ListHeader({ $target, initialState }) {
+  /**
+   * state : {isLoading}
+   */
+  this.state = initialState;
+  const $header = document.createElement("h1");
+
+  $target.appendChild($header);
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+
+  this.render = () => {
+    if (this.state) {
+      $header.innerText = "Loading중...";
+    } else {
+      $header.innerText = "호민의 Notion";
+    }
+  };
+
+  this.render();
+}
