aec0bf2166c43e8f1dc48232928f842bb55c9d4a
yejinleee <yejin9487@gmail.com>
2023-10-26 12:56:10 +0900

    feat: 없는 페이지 접근시 confirm

--- src/utils/api.js
+++ src/utils/api.js
@@ -8,7 +8,10 @@ export const request = async (url, options = {}) => {
     if (res.ok) {
       return await res.json(); //await가 없으면 Promise가 리턴됩니다. 굳이 asyncawait랑 프로미스를 섞을 필요없음
     } else {
-      throw new Error("API 처리 중 오류");
+      if (window.confirm("존재하지 않는 페이지 입니다")) {
+        history.replaceState(null, null, "/");
+      }
+      // throw new Error("API 처리 중 오류");
     }
   } catch (e) {
     console.log(e);
