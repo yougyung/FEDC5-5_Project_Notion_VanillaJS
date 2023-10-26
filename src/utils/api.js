5f562c73eaf71e87fcc3e31b531d3d411f2504cd
lja0395 <kittyja1023@naver.com>
2023-10-26 12:38:33 +0900

    feat: 컴포넌트 생성, utils함수 생성

---
+++ src/utils/api.js
@@ -0,0 +1,21 @@
+export const API_END_POINT = 'https://kdt-frontend.programmers.co.kr'
+
+export const request = async (url, options = {}) => {
+  try {
+    const res = await fetch(url, {
+      ...options,
+      headers: {
+        'Content-type': 'application/json',
+        'x-username': 'kitty',
+      },
+    });
+
+    if (res.ok) {
+      return await res.json();
+    }
+
+    throw new Error('요청에 실패했습니다.');
+  } catch (e) {
+    console.error(e);
+  }
+};
