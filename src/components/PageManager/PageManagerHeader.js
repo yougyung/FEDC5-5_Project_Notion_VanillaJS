0ac50ef3d4b14478a3b5e7c0992ad1130734c791
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2023-10-26 23:03:48 +0900

    design: svg 수정

--- src/components/PageManager/PageManagerHeader.js
+++ src/components/PageManager/PageManagerHeader.js
@@ -1,12 +1,9 @@
 export default function PageManagerHeader({ $target, onNewPageAdd }) {
   const $pageAddButton = document.createElement("button");
   $pageAddButton.className = "page_manager_add_button";
-  $pageAddButton.innerHTML = `
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  $pageAddButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="black" height="10" width="10">
   <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-</svg>
-
-  `;
+</svg>`;
   $target.appendChild($pageAddButton);
 
   $pageAddButton.addEventListener("click", async (e) => {
