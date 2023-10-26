8bcd10bbd0288d70ec25d5fd494d2410537a8244
lja0395 <kittyja1023@naver.com>
2023-10-26 21:41:33 +0900

    feat: DocumentEditPage, Editor 컴포넌트 구현

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,37 @@
 import Sidebar from './Sidebar.js';
+import DocumentEditPage from './DocumentEditPage.js';
+import { ROUTE_DOCUMENTS } from '../utils/contants.js';
+import { isNew } from '../utils/helper.js';
+import { initRouter } from '../utils/router.js';
 
- export default function App({ $target }) {
+export default function App({ $target }) {
    isNew(new.target);
 
    new Sidebar({
      $target,
    });
- }
+
+   const documentEditPage = new DocumentEditPage({
+     $target,
+     initialState: {
+       documentId: null,
+       document: {
+         title: '',
+         content: '',
+       },
+     },
+   });
+
+   this.route = () => {
+     const { pathname } = window.location;
+
+     if (pathname.indexOf(ROUTE_DOCUMENTS) === 0) {
+       const [, , documentId] = pathname.split('/');
+       documentEditPage.setState({ documentId });
+     }
+   };
+
+   this.route();
+
+   initRouter(() => this.route());
+}
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,17 @@
 export const ROUTE_DOCUMENTS = '/documents';
- export const NEW = 'new';
+export const NEW = 'new';
+
+export const MESSAGE = {
+    DO_NOT_DELETE_FIRST_PAGE: '첫 페이지는 지우지 말아주세요',
+    DELETE_PAGE: '페이지를 삭제하시겠습니까?',
+    NOT_NEW_INSTANCE: 'new 없이 호출됨',
+    REDIRECT: '존재하지 않는 페이지입니다. 첫 페이지로 이동합니다.',
+    WRITE_CONTENT: '내용을 입력하세요',
+};
+
+export const TEXT = {
+    ADD_PAGE: '페이지 추가',
+    NEW_PAGE: '새 페이지',
+    UNTITLED: '제목 없음',
+};
+  
