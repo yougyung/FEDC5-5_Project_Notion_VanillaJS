8f6c2c2c2ed178bb3674068a03198ac8d33d0685
Kim-Jaemin420 <jany240@naver.com>
2023-10-26 13:20:15 +0900

    Feat: app 돔이 없을 경우 화면에 에러 표시

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,11 @@ import App from "./App";
 export function render() {
   const $app = document.getElementById("app");
 
-  if (!$app) throw new Error("비정상적인 접근입니다.");
+  if (!$app) {
+    document.body.innerHTML = "애플리케이션을 불러오는 데 문제가 발생했습니다.";
+
+    throw new Error("애플리케이션을 불러오는 데 문제가 발생했습니다.");
+  }
 
   const componentInstance = createComponent(App);
   $app.innerHTML = componentInstance.element;
