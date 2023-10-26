2a108d17122e0208a9dad0492c18d053ea2fbbb6
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2023-10-26 23:24:10 +0900

    design: 버튼 style 수정

--- src/components/PageManager/PageList.js
+++ src/components/PageManager/PageList.js
@@ -25,19 +25,19 @@ export default function PageList({
             <li data-id=${page.id} class='page_item'>
               <div class='item_seperator'>
                 <div class='toggle_button'>
-                ${`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="toggle_svg" stroke-width="1.5" stroke="gray" width="1rem" height="1rem">
+                ${`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="toggle_svg" stroke-width="1.5" stroke="#64635E" width="15" height="15">
         <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
       </svg>`} 
                   </div>
                 <div class='item_title'>${page.title}</div>
                 <div class='button_group'>
                   <div class='delete_button'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" width="1rem" height="1rem">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#64635E" width="15" height="15">
   <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
 </svg>
                   </div>
                   <div class='add_button'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="white" width="1rem" height="1rem">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="#64635E" width="15" height="15">
   <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
 </svg>
                   </div>
--- src/components/PageManager/PageManagerHeader.js
+++ src/components/PageManager/PageManagerHeader.js
@@ -1,7 +1,7 @@
 export default function PageManagerHeader({ $target, onNewPageAdd }) {
-  const $pageAddButton = document.createElement("button");
+  const $pageAddButton = document.createElement("div");
   $pageAddButton.className = "page_manager_add_button";
-  $pageAddButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="black" height="10" width="10">
+  $pageAddButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="#64635E" height="15" width="15">
   <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
 </svg>`;
   $target.appendChild($pageAddButton);
