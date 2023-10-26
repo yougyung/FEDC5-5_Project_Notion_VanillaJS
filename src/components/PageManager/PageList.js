7895e1d0272e7c94b6aa295e95325a6ed4114f66
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2023-10-26 13:30:53 +0900

    feat: 토글 버튼 기능 추가

--- src/components/PageManager/PageList.js
+++ src/components/PageManager/PageList.js
@@ -17,7 +17,7 @@ export default function PageList({
   };
   const renderPageSubTree = (pageTree, isRoot = false) => {
     return `
-      <ul style='${isRoot ? "display:block" : "display:none"}'>
+      <ul >
         ${pageTree
           .map(
             (page) =>
@@ -59,16 +59,12 @@ export default function PageList({
                   </button>
                 <div class='item_title'>${page.title}</div>
                 <div class='button_group'>
-                  <button class='delete_button'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                    </svg>
-                  </button>
-                  <button class='add_button'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                    </svg>
-                  </button>
+                  <div class='delete_button'>
+                    x
+                  </div>
+                  <div class='add_button'>
+                    +
+                  </div>
                 </div>
               </div>
               ${
@@ -108,6 +104,14 @@ export default function PageList({
     } else if (className === "delete_button") {
       onPageDelete(id);
     } else if (className === "add_button") {
+      const $ul = $li.querySelector("ul");
+      if ($ul) {
+        if ($ul && $ul.style.display === "none") {
+          $ul.style.display = "block";
+        } else {
+          $ul.style.display = "none";
+        }
+      }
       onSubPageAdd(id);
     }
     console.log(id);
