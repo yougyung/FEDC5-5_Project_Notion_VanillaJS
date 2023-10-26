27b1f4c391954c1bb496ad264c13fa6d3241d64e
HoberMin <102784200+HoberMin@users.noreply.github.com>
2023-10-26 21:42:34 +0900

    feat : keyup이벤트에 따른 일시적 저장 - 로컬스토리지 활용

--- src/page/EditorPage.js
+++ src/page/EditorPage.js
@@ -8,6 +8,13 @@ export default function EditorPage({ $target, initialState, onListRender }) {
   const $eidtor = document.createElement("div");
   $eidtor.classList = "editor-div";
   this.state = initialState;
+  let docLocalSaveKey = `temp-doc-content-${this.state.selectedID}`;
+
+  const post = getItem(docLocalSaveKey, {
+    title: "",
+    content: "",
+  });
+
   const editHeader = new EditHeader({
     $target: $eidtor,
     initialState: { ...this.state, isLoading: false },
@@ -15,9 +22,29 @@ export default function EditorPage({ $target, initialState, onListRender }) {
   const editor = new Editor({
     $target: $eidtor,
     initialState: post,
+      setItem(docLocalSaveKey, {
+        ...post,
+        tempSaveDate: new Date(),
+      });
+        removeItem(docLocalSaveKey);
     },
   });
+    if (this.state.selectedID !== nextState.selectedID) {
+      docLocalSaveKey = `temp-doc-content-${nextState.selectedID}`;
   this.render = () => {
     $target.appendChild($eidtor);
   };
+      if (
+        tempPost.tempSaveDate &&
+        tempPost.tempSaveDate > selectedDocument.updatedAt
+      ) {
+        if (confirm("저장되지 않은 임시데이터가 있습니다. 불러올까요?")) {
+          this.setState({
+            ...this.state,
+            selectedDocument: tempPost,
+            isLoading: false,
+          });
+          return;
+        }
+      }
 }
