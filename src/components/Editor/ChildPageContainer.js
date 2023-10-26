892779cb3e4cf1fdca8483fdbb01314a825a48dd
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2023-10-26 22:47:45 +0900

    feat: editor 아래 child page 링크 컨테이너 구현

---
+++ src/components/Editor/ChildPageContainer.js
@@ -0,0 +1,35 @@
+import { push } from "../../router.js";
+
+export default function ChildPageContainer({
+  $target,
+  initialState,
+  onSubPageClick,
+}) {
+  const $childPageContainer = document.createElement("ul");
+  $childPageContainer.className = "child_page_container";
+  this.state = initialState;
+  $target.appendChild($childPageContainer);
+
+  this.render = () => {
+    //console.log(this.state);
+    $childPageContainer.innerHTML = `
+    ${this.state.map(
+      (page) =>
+        `<li data-id=${page.id} class='child_page'><span>${page.title}</span></li>`
+    )}
+    
+  `;
+  };
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+  this.render();
+  $childPageContainer.addEventListener("click", (e) => {
+    //console.log(e.target.closest("li").dataset.id);
+    const { id } = e.target.closest("li").dataset;
+    this.state.forEach((page) => {
+      if (+page.id === +id) onSubPageClick(page);
+    });
+  });
+}
