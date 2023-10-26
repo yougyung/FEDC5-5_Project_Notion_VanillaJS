d5f6763f3bffaf152296d29841f2783d323e8a1a
JangGwon <tlsakch510@naver.com>
2023-10-26 23:56:35 +0900

    style : notfoundPage 디자인

--- src/component/main/tempPage/notdataPage.js
+++ src/component/main/tempPage/notdataPage.js
@@ -4,9 +4,11 @@ export default function notDatapage() {
 
     function init() {
         pageElement.style.display = "none";
-        pageElement.innerHTML = `<div>
+        pageElement.innerHTML = `<div class="notfoundpage">
+         <div class="notfoundcontent">
             <h1> 404 Not Found. </h1>
-            <h3> 해당 페이지가 삭제되었습니다. <h
+            <h3> 해당 페이지가 삭제되었습니다. </h3>
+            </div>
         </div>`
     }
 
