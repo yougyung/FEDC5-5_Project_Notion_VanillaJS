b5de5116b1ab4e8ab8cd991969bda790d6039eac
Suyoung Shin <joyswim.dev@gmail.com>
2023-10-26 13:08:11 +0900

    feat: 편집기 자동 저장 기능

--- src/components/EditPage.js
+++ src/components/EditPage.js
@@ -1,4 +1,6 @@
 import Editor from './Editor.js'
+import { setItem } from '../utils/storage.js'
+
 
 const EDITOR_DUMMY = {
   title: '처음 작성하는 제목',
@@ -9,10 +11,34 @@ export default function EditPage({ $target, initialState }) {
   const $page = document.createElement('div')
   $page.id = "document-contents"
 
+  // {documentId, document={title, content}}
+  this.state = initialState
+
+  let localSaveKey = `temp-document-${this.state.documentId}`
+
+  let timer = null
+
+  // Editor 컴포넌트 생성
   new Editor({
     $target: $page,
-    initialState: EDITOR_DUMMY
+    initialState: EDITOR_DUMMY,
+    onEditing: (document) => {
+      // document {title, content}
+      if (timer !== null) {
+        clearTimeout(timer)
+      }
+      timer = setTimeout(async() => {
+        setItem(localSaveKey, {
+          ...document,
+          tempSaveDate: new Date()
+        })
+      }, 1000)
+    }
   })
+
+  this.setState = (nextState) => {
+    
+  }
 
   this.render = () => {
     $target.appendChild($page)
