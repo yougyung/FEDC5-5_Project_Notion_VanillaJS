f1d9ce452a7a34c6fa343463042033481bcdda39
Suyoung Shin <joyswim.dev@gmail.com>
2023-10-26 13:06:40 +0900

    feat: 기본 Editor 추가

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,21 +1,48 @@
-export default function Editor({ $target, initialState }) {
+export default function Editor({ $target, initialState = {
+  title: '',
+  content: ''
+}, onEditing }) {
   const $editor = document.createElement('div')
   $editor.className = 'editor-wrap'
 
-  const { title, content } = initialState
+  let isInitialize = false
+  // {title, content}
+  this.state = initialState
+
+  $target.appendChild($editor)
 
   this.setState = (nextState) => {
-    title = nextState.title
-    content = nextState.content
+    this.state = nextState
+    $editor.querySelector('[name=title]').value = this.state.title
+    $editor.querySelector('[name=content]').value = this.state.content
     this.render()
   }
 
   this.render = () => {
-    $editor.innerHTML = `
-      <input class="editor-title" type="text" name="title" style="width: 600px;" placeholder="제목 없음" value="${title}" />
-      <textarea class="editor-content" name="content" style="width: 600px; height: 400px;" placeholder="내용을 입력하세요">${content}</textarea>
-    `
-    $target.appendChild($editor)
+    const { title, content } = this.state
+    if (!isInitialize) {
+      $editor.innerHTML = `
+        <input class="editor-title" type="text" name="title" style="width: 600px;" placeholder="제목 없음" value="${title}" />
+        <textarea class="editor-content" name="content" style="width: 600px; height: 400px;" placeholder="내용을 입력하세요">${content}</textarea>
+      `
+      isInitialize = true
+    }
   }
+
   this.render()
+
+  $editor.addEventListener('keyup', (e) => {
+    const { target } = e
+    const name = target.getAttribute('name')
+
+    if (this.state[name] !== undefined) {
+      const nextState = {
+        ...this.state,
+        [name]: target.value
+      }
+      
+      this.setState(nextState)
+      onEditing(this.state)
+    }
+  })
 }
