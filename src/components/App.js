ed55565f26fa47763f6eaa3912000b1b53165dc0
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2023-10-27 22:54:46 +0900

    refactor: Sidebar 구조 수정

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,74 @@
-import SideBar from "../layout/SideBar.js";
+import Sidebar from "../layout/Sidebar.js";
 import EditDocument from "../layout/EditDocument.js";
+import {
+  getAllDocumentLists,
+  getDocumentContent,
+  createDocument,
+} from "../api/document.js";
 
 export default function App({ $target }) {
   this.state = {
     documents: [], // sidebar 문서 목록
-    selectedDocument: [], // editor에 보여줄 특정 문서
+    selectedDocument: {
+      id: 0,
+      title: "",
+      createdAt: "",
+      updatedAt: "",
+    }, // editor에 보여줄 특정 문서
   };
 
   this.setState = (nextState) => {
     this.state = nextState;
   };
 
-  const sidebar = new SideBar({
+  /** 문서 목록 가져오기: state 변경, 사이드바 리렌더링 */
+  const fetchDocumentLists = async () => {
+    const documents = await getAllDocumentLists();
+
+    this.setState({
+      ...this.state,
+      documents,
+    });
+
+    sidebar.setState(this.state.documents);
+  };
+
+  /** 선택된 문서 가져오기 */
+  const fetchDocument = async (documentId) => {
+    const selectedDocument = await getPost(documentId);
+    this.setState({
+      ...this.state,
+      selectedDocument,
+    });
+  };
+
+  /** 새로운 문서 생성하기 */
+  const onAddNewDocument = async (parentId) => {
+    const newDocument = await createDocument({
+      title: "제목없음",
+      parent: parentId,
+    });
+
+    console.log(newDocument);
+    const newDocumentBody = await getDocumentContent(newDocument.id);
+  };
+
+  const init = async () => {
+    await fetchDocumentLists();
+  };
+
+  // Sidebar
+  const sidebar = new Sidebar({
     $target,
     initialState: this.state.documents,
+    onAddNewDocument,
   });
 
+  // EditorDocument
   const editDocument = new EditDocument({
     $target,
     initialState: this.state,
   });
+
+  init();
 }
--- src/layout/SideBar.js
+++ src/layout/SideBar.js
@@ -1,79 +1,75 @@
 import { getAllDocumentLists, createDocument } from "../api/document.js";
 
-export default function SideBar({ $target, initialState }) {
+export default function Sidebar({ $target, initialState, onAddNewDocument }) {
   const $sideContainer = document.createElement("div");
-  $sideContainer.className = "side-container";
+  $sideContainer.classList.add("side-container");
   $target.appendChild($sideContainer);
 
-  this.state = {
-    document: initialState,
-    isToggle: false,
+  this.state = initialState;
+
+  const toggleDocument = (element) => {
+    const $leafDocuments = element.querySelector(".side-bar-leaf-documents");
+    const isVisible = $leafDocuments.style.display === "block";
+    $leafDocuments.style.display = isVisible ? "none" : "block";
   };
 
-  const setState = (nextState) => {
+  const renderChildDocuments = (childDocuments) => {
+    if (childDocuments && childDocuments.length > 0) {
+      return `<div class="side-bar-leaf-documents" style="display: none;">
+        ${childDocuments.map(renderDocuments).join("")}
+      </div>`;
+    } else {
+      return '<div class="side-bar-leaf-documents" style="display: none;">하위 문서가 없습니다.</div>';
+    }
+  };
+
+  const renderDocuments = (document) => {
+    const { id, title, documents: childDocuments } = document;
+    const childDocumentsHTML = renderChildDocuments(childDocuments);
+
+    return `
+      <div class="side-bar-document" data-id="${id}">
+        <img class="toggle-button" src="src/assets/arrow.png" alt="toggle button">
+        ${title}
+        <img class="add-button" src="src/assets/add.png"/>
+        <img class="delete-button" src="src/assets/delete.png"/>
+        ${childDocumentsHTML}
+  </div>
+`;
+  };
+
+  this.setState = (nextState) => {
     this.state = nextState;
-    render();
+    this.render();
   };
 
-  const initDocument = async () => {
-    const documentLists = await getAllDocumentLists();
-    setState({ ...this.state, documents: documentLists });
+  const handleButtonClick = (e) => {
+    const $node = e.target;
+    const $parentDocument = $node.closest(".side-bar-document");
+    const { id } = $parentDocument.dataset;
+
+    if ($node.classList.contains("toggle-button")) {
+      toggleDocument($parentDocument);
+    } else if ($node.classList.contains("add-button")) {
+      console.log(id);
+      onAddNewDocument(id);
+    } else if ($node.classList.contains("delete-button")) {
+      // 삭제 기능
+    }
   };
 
-  const renderDocumentLists = (documents) => {
-    if (!documents) return "";
-
-    return documents
-      .map(({ id, title, documents: childDocuments }) => {
-        let childDocumentsHTML = "";
-        if (childDocuments && childDocuments.length > 0) {
-          childDocumentsHTML = `
-              <div class="side-bar-leaf-documents" style="display: none;">
-                ${renderDocumentLists(childDocuments)}
-              </div>
-            `;
-        }
-
-        return `
-            <div class="side-bar-document">
-              <button class="toggle-button">></button>
-              ${title}
-              <button class="add-button">+</button>
-              <button class="delete-button">-</button>
-              ${childDocumentsHTML}
-            </div>
-          `;
-      })
-      .join("");
-  };
-
-  const render = () => {
-    const documentLists = renderDocumentLists(this.state.documents);
+  this.render = () => {
+    const documentListHTML = this.state.map(renderDocuments).join("");
 
     $sideContainer.innerHTML = `
       <div class="side-bar-container">
         <h3 class="side-bar-header-title">김윤경의 Notion</h3>
-          ${documentLists}
+        ${documentListHTML}
       </div>
     `;
   };
 
-  $sideContainer.addEventListener("click", (e) => {
-    const $node = e.target;
+  $sideContainer.addEventListener("click", handleButtonClick);
 
-    if ($node.matches(".toggle-button")) {
-      const $leafDocuments = $node.parentNode.querySelector(
-        ".side-bar-leaf-documents"
-      );
-      $leafDocuments.style.display =
-        $leafDocuments.style.display === "none" ? "block" : "none";
-    } else if ($node.matches(".add-button")) {
-      // 클릭한 Document의 하위 Document로 새 Document를 생성하고 편집화면으로 넘기는 코드
-    } else if ($node.matches(".delete-button")) {
-      // 클릭한 Document를 삭제하는 코드
-    }
-  });
-
-  initDocument();
-  render();
+  this.render();
 }
