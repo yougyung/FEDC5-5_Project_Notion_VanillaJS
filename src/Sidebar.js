cfabcf37ea1f73bf9078f647245035dfcb2ab080
최훈오 <gnsdh86166@gmail.com>
2023-10-27 13:56:27 +0900

    [Refactor] 이미지 업로드 방식 변경

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -29,30 +29,7 @@ export default function Sidebar({ $target, initialState }) {
 
   this.setState = async () => {
     const documents = await request('');
-    sidebarList.setState([
-      {
-        id: 1, // Document id
-        title: '노션을 만들자', // Document title
-        documents: [
-          {
-            id: 2,
-            title: '블라블라',
-            documents: [
-              {
-                id: 3,
-                title: '함냐함냐',
-                documents: [],
-              },
-            ],
-          },
-        ],
-      },
-      {
-        id: 4,
-        title: 'hello!',
-        documents: [],
-      },
-    ]);
+    sidebarList.setState(documents);
     this.render();
   };
 
