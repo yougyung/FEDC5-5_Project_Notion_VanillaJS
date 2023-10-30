81fa2b626bf3e301ce06e6f51d7c81040f7e8c86
Seongbin Kim <seongbin9786@gmail.com>
2023-10-30 19:53:38 +0900

    사이드바에서 각 문서 제목 클릭 시 이동되게

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,9 +3,19 @@ import { $ } from "../shared/$.js";
 const username = "Seongbin Kim의 Notion";
 const userEmail = "seongbin9786@uos.ac.kr";
 
-const DocumentListItem = ({ title, documents }) => $`
+const goTo = (id) => {
+    history.pushState(null, "", `/documents/${id}`);
+    window.dispatchEvent(new Event("popstate"));
+};
+
+const DocumentListItem = ({ id, title, documents }) => $`
     <div className=document_list_item>
-        <div className=document_list_item__title>${title === "" ? "제목 없음" : title}</div>
+        <div 
+            className=document_list_item__title
+            onclick=${() => goTo(id)}
+        >
+            ${title === "" ? "제목 없음" : title}
+        </div>
         ${documents.length === 0 ? null : documents.map(DocumentListItem)}
     </div>
 `;
