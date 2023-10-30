8c5f0da6ec7450960d221bfb3af17bdbcd7ae781
Seongbin Kim <seongbin9786@gmail.com>
2023-10-30 18:07:15 +0900

    App 코드 정리 간단히

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,14 @@
 import { Layout } from "./Layout.js";
 
-// 이제 App은 최초 로딩 책임이 생김. 굿.
 export const App = async () => {
-    const $body = document.getElementsByTagName("body").item(0);
-
-    const rootDocumentList = await window.api.list();
-
+    // 이제 App은 최초 로딩 책임이 생김. 굿.
     // 기본으로 목록 중 첫 문서를 현재 문서로 사용
+    const rootDocumentList = await window.api.list();
     const currentDocument = await window.api.content(rootDocumentList[0].id);
-
     console.log(rootDocumentList, currentDocument);
 
+    // 구성 요소 마운트
     const $layout = Layout(rootDocumentList, currentDocument);
-
+    const $body = document.getElementsByTagName("body").item(0);
     $body.appendChild($layout);
-
-    return $body;
 };
