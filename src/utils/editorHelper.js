9813740061ed75e2369799c546f8454cb6d589a8
Zoody <reopard506@gmail.com>
2023-11-06 22:16:32 +0900

    [REFACT] 정규표현식 수행 간소화

--- src/utils/editorHelper.js
+++ src/utils/editorHelper.js
@@ -5,42 +5,31 @@ export function parseContent(content) {
   if (!content) return "";
 
   // Regular Expressions
-  const h1 = /^#{1}[^#].*$/gm;
-  const h2 = /^#{2}[^#].*$/gm;
-  const h3 = /^#{3}[^#].*$/gm;
+  const h1 = /^#{1}[^#](.*)$/gm;
+  const h2 = /^#{2}[^#](.*)$/gm;
+  const h3 = /^#{3}[^#](.*)$/gm;
   const bold = /\*\*[^\*\n]+\*\*/gm;
   const italics = /\*([^*]+)\*/gm;
   const link = /\[[^\]]*\]\([^)]*\)/gm;
 
   // # -> Heading 1
-  if (h1.test(content)) {
-    const matches = content.match(h1);
+  // case1
+  // if (h1.test(content)) {
+  //   const matches = content.match(h1);
 
-    matches.forEach((element) => {
-      const extractedText = element.slice(1);
-      content = content.replace(element, "<h1>" + extractedText + "</h1>");
-    });
-  }
+  //   matches.forEach((element) => {
+  //     const extractedText = element.slice(1);
+  //     content = content.replace(element, "<h1>" + extractedText + "</h1>");
+  //   });
+  // }
+  // case2
+  content = content.replace(h1, "<h1>$1</h1>");
 
   // ## -> Heading 2
-  if (h2.test(content)) {
-    const matches = content.match(h2);
-
-    matches.forEach((element) => {
-      const extractedText = element.slice(2);
-      content = content.replace(element, "<h2>" + extractedText + "</h2>");
-    });
-  }
+  content = content.replace(h2, "<h2>$1</h2>");
 
   // ### -> Heading 3
-  if (h3.test(content)) {
-    const matches = content.match(h3);
-
-    matches.forEach((element) => {
-      const extractedText = element.slice(3);
-      content = content.replace(element, "<h3>" + extractedText + "</h3>");
-    });
-  }
+  content = content.replace(h3, "<h3>$1</h3>");
 
   // **Bold**
   if (bold.test(content)) {
