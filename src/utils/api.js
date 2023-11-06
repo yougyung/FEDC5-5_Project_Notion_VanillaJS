b5584341edbe42be5cd6bddbbef97c31aca8a6bb
YunSukHyun <czcz9207@gmail.com>
2023-11-06 16:32:35 +0900

    fix: const

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
-const API_END_POINT = "https://kdt-frontend.programmers.co.kr";
+const API_END_POINT = "https://kdt-frontend.programmers.co.kr/documents/";
 
 export const request = async (url = "", options = {}) => {
   try {
-    const response = await fetch(`${API_END_POINT}/documents/${url}`, {
+    const response = await fetch(`${API_END_POINT}${url}`, {
       ...options,
       headers: {
         "Content-Type": "application/json",
