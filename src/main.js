13e1c4ad314133b20ff42643fe4980ef8ad1a090
loevray <rei980213@gmail.com>
2023-11-06 21:02:02 +0900

    Feat: Add globalStore in main

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import App from "./App.js";
 import documentsReducer from "./modules/document.js";
 import Observer from "./utils/observer/Observable.js";
 const observer = Object.freeze(new Observer());
-export default observer;
+const store = createStore(documentsReducer);
+export { store, observer };
 const $root = document.getElementById("root");
 new App({ $target: $root });
