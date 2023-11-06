ca1320c2daf29fa51caa29c02a1615a30134c082
JangGwon <tlsakch510@naver.com>
2023-11-06 18:30:08 +0900

    rename : api.js에 변수명 수정 - res-> result

--- src/api.js
+++ src/api.js
@@ -2,15 +2,15 @@
 import { API_END_POINT, X_USERNAME } from "../env.js"
 
 export default async function request(url, id) {
-    const res = await fetch(`${API_END_POINT}${url}`, {
+    const result = await fetch(`${API_END_POINT}${url}`, {
         ...id,
         headers: {
             'x-username': X_USERNAME,
             'Content-Type': 'application/json'
         }
     })
-    if (res.ok) {
-        return res.json();
+    if (result.ok) {
+        return result.json();
     }
 }
 
