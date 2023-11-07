78e198c162e7d52d89b17915c096b171252f866d
Cho-Ik-Jun <harry960629@naver.com>
2023-11-07 22:48:27 +0900

    ➖ comment: 데이터 구조 주석 제거

--- src/api.js
+++ src/api.js
@@ -19,35 +19,3 @@ export async function request(url, options = {}) {
     alert(e.message);
   }
 }
-
-/* 데이터 구조
-[
-  {
-    id: 1, // Document id
-    title: '노션을 만들자', // Document title
-    documents: [
-      {
-        id: 2,
-        title: '블라블라',
-        documents: [
-          {
-            id: 3,
-            title: '함냐함냐',
-            documents: [],
-          },
-          {
-            id: 5,
-            title: '함냐함냐2',
-            documents: [],
-          },
-        ],
-      },
-    ],
-  },
-  {
-    id: 4,
-    title: 'hello!',
-    documents: [],
-  },
-];
-*/
