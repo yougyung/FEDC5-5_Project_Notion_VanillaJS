ff45b115eee62b3f4e51b492e887c3d739142ed6
Kim-Jaemin420 <jany240@naver.com>
2023-11-07 14:31:35 +0900

    Modify: 불필요한 IIFE 제거

--- src/core/hooks/useState.ts
+++ src/core/hooks/useState.ts
@@ -5,42 +5,40 @@ interface SetState<T> {
   (value: T | ((previousState: T) => T)): void;
 }
 
-const useState = (() => {
-  const componentsState: Record<string, unknown[]> = {};
+const componentsState: Record<string, unknown[]> = {};
 
-  return function <T>(initialValue: T): [T, SetState<T>] {
-    if (!currentComponent) {
-      throw new Error("useState는 컴포넌트 안에서만 호출될 수 있습니다.");
+function useState<T>(initialValue: T): [T, SetState<T>] {
+  if (!currentComponent) {
+    throw new Error("useState는 컴포넌트 안에서만 호출될 수 있습니다.");
+  }
+
+  const { id, stateIndex } = currentComponent;
+
+  if (!componentsState[id]) {
+    componentsState[id] = [];
+  }
+
+  if (componentsState[id][stateIndex] === undefined) {
+    componentsState[id][stateIndex] = initialValue;
+  }
+
+  const setState: SetState<T> = (newValue) => {
+    const currentState = componentsState[id][stateIndex] as T;
+
+    const updatedState =
+      typeof newValue === "function" ? (newValue as (previousValue: T) => T)(currentState) : newValue;
+
+    if (currentState !== updatedState) {
+      componentsState[id][stateIndex] = updatedState;
+      render();
     }
-
-    const { id, stateIndex } = currentComponent;
-
-    if (!componentsState[id]) {
-      componentsState[id] = [];
-    }
-
-    if (componentsState[id][stateIndex] === undefined) {
-      componentsState[id][stateIndex] = initialValue;
-    }
-
-    const setState: SetState<T> = (newValue) => {
-      const currentState = componentsState[id][stateIndex] as T;
-
-      const updatedState =
-        typeof newValue === "function" ? (newValue as (previousValue: T) => T)(currentState) : newValue;
-
-      if (currentState !== updatedState) {
-        componentsState[id][stateIndex] = updatedState;
-        render();
-      }
-    };
-
-    const stateValue = componentsState[id][stateIndex] as T;
-
-    currentComponent.stateIndex += 1;
-
-    return [stateValue, setState];
   };
-})();
+
+  const stateValue = componentsState[id][stateIndex] as T;
+
+  currentComponent.stateIndex += 1;
+
+  return [stateValue, setState];
+}
 
 export default useState;
