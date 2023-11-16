a00771bf5b58d59335cea3b2e4e29e0db5cb2a28
타래 <85001878+meowTarae@users.noreply.github.com>
2023-11-16 18:57:53 +0900

    ✨ Feat: '예외 처리 추가'

--- src/utils/api.js
+++ src/utils/api.js
@@ -10,9 +10,29 @@ export const request = async (url, options = {}) => {
       },
     });
 
-    if (res.ok) {
-      return await res.json();
+    if (!res.ok) {
+      const { status } = res;
+      if (status >= 400 && status < 500) {
+        // 클라이언트 관련 에러 처리
+        switch (status) {
+          case 404:
+            console.error("Client error: Invalid approach", status);
+            break;
+
+          default:
+            console.error("Client error", status);
+            break;
+        }
+      } else if (status >= 500) {
+        // 서버 관련 에러 처리
+        console.error("Server error", status);
+      } else {
+        // 기타 에러 처리
+        console.error("Unexpected error", status);
+      }
     }
+
+    return await res.json();
   } catch (e) {
     throw new Error(`API Error: ${e.message}`);
   }
