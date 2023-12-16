f80d76770f5d0507c398400bad2c997294e075af
lunarmoon7 <49crehbgr@gmail.com>
2023-12-16 15:33:39 +0900

    fix: createDOM 인자명 오류 수정 - tag -> tagName

--- src/components/Document/DocumentFooter.js
+++ src/components/Document/DocumentFooter.js
@@ -3,30 +3,31 @@ import Modal from '../common/Modal.js';
 
 export default function DocumentFooter({ $target, onOpen }) {
   const $documentFooter = createDOM({
-    tag: 'div',
+    tagName: 'div',
     className: 'document-footer',
   });
   const $helpButton = createDOM({
-    tag: 'button',
+    tagName: 'button',
     className: 'help-button',
     innerHTML: '<i class="fa-regular fa-circle-question"></i>',
   });
 
   const $socialGroup = createDOM({ tag: 'div', className: 'social-group' });
   const $githubButton = createDOM({
-    tag: 'button',
+    tagName: 'button',
     className: 'github-button',
     innerHTML:
       '<a href="https://github.com/lunarmoon7" target="_blank"><i class="fa-brands fa-github"></i></a>',
   });
   const $velogButton = createDOM({
-    tag: 'button',
+    tagName: 'button',
     className: 'velog-button',
     innerHTML:
       '<a href="https://velog.io/@49crehbgr" target="_blank"><i class="fa-brands fa-vimeo"></i></a>',
   });
 
   appendChildAll($socialGroup, [$githubButton, $velogButton]);
+  appendChildAll($documentFooter, [$helpButton, $socialGroup]);
   // appendChildAll($documentFooter, [$helpButton, $socialGroup]);
   appendChildAll($target, [$documentFooter]);
 
